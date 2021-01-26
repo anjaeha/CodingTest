0aa43379e45df7b9f89068857122331ab7064863
anjaeha <vix849@naver.com>
2021-01-26 12:14:11 +0900

    2021.01.26

--- testcode.py
+++ testcode.py
@@ -1,15 +1,10 @@
-T = int(input())
+T = input()
+answer = 0
 
-for case in range(T):
-    answer = 0
-    num = input().split()
-    N = float(num[0])
-    for i in num:
-        if i == '@':
-            N = N * 3
-        elif i == '%':
-            N = N + 5
-        elif i == '#':
-            N = N - 7
-    
-    print("%.2f" % N)
+for i in range(1, len(T)):
+    if T[i-1] == T[i]:
+        answer += 5
+    else:
+        answer += 10
+
+print(answer + 10)
