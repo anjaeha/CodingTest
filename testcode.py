46a5c26fb3b22fec8a1752a7d0208d3ed6abcc99
anjaeha <vix849@naver.com>
2021-01-28 11:53:40 +0900

    2021.01.28

--- testcode.py
+++
@@ -1,8 +0,0 @@
-def solution(participant, completion):
-    answer = participant - completion
-    return answer
-
-participant = [leo, kiki, eden]
-completion = [eden, kiki]
-
-solution(participant, completion)
