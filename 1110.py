0056abd4b62969f2f6916a5cc6c723275a8db966
anjaeha <vix849@gmail.com>
2021-02-10 11:49:32 +0900

    2021.02.10

---
+++ 1110.py
@@ -0,0 +1,21 @@
+num = int(input())
+
+cnt = 0
+
+new_num = 0
+temp = 0
+
+check = num
+
+
+while True:
+    temp = (num // 10) + (num % 10)
+    new_num = (num % 10) * 10 + (temp % 10)
+    cnt += 1
+
+    num = new_num
+    
+    if num == check:
+        break
+
+print(cnt)
