68bf17e08e3b7d92b442612cd75eea405cb4369d
anjaeha <vix849@gmail.com>
2021-02-16 15:15:17 +0900

    2021.02.16

--- 2178_.py
+++ 2178_.py
@@ -1,31 +1,31 @@
 from collections import deque
 queue = deque()
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-
 n, m = map(int, input().split())
 
+
 t = []
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
 
 for i in range(n):
     t.append(list(map(int, input())))
 
 
-def bfs(x, y):
+def bfs(x, y):    
     queue.append((x, y))
-    
+
     while queue:
-        a, b = queue.popleft()
+        x, y = queue.popleft()
 
         for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
             if 0 <= nx < n and 0 <= ny < m and t[nx][ny] == 1:
-                t[nx][ny] = t[a][b] + 1
+                t[nx][ny] = t[x][y] + 1
                 queue.append((nx, ny))
-
     return t[n-1][m-1]
 
 print(bfs(0,0))
+print(t)
+
