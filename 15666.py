10188e75c2ef1bafda3acd7e4b76e1d8e7d0959b
anjaeha <vix849@gmail.com>
2021-02-19 23:52:28 +0900

    2021.02.19

--- 15666.py
+++ 15666.py
@@ -1,32 +1,32 @@
 n, m = map(int, input().split())
 
-
 num = list(map(int, input().split()))
 num.sort()
 check = [False] * n
+
 arr = []
 answer = []
 
 def dfs(cnt):
-    if(cnt == m):
-        answer.append((tuple(arr)))
+    if (cnt == m):
+        answer.append(tuple(arr))
         return
-    
+
     for i in range(n):
+
         if(check[i]):
             continue
 
-        check[i] = True
-        arr.append(num[i])
         
+        arr.append(num[i])
         dfs(cnt+1)
+        check[i] = True
         arr.pop()
-
         for j in range(i+1, n):
             check[j] = False
 
 
 dfs(0)
 
-for x in sorted(list(set(answer))):
-    print(*x)
+for i in sorted(list(set(answer))):
+    print(*i)
