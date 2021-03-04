e23f7790522e4c9ba9503db2b6b69bf2a05bf5d3
anjaeha <vix849@gmail.com>
2021-03-04 11:41:29 +0900

    2021.03.04

--- 1181.py
+++ 1181.py
@@ -1,19 +1,15 @@
-N = int(input())
+n = int(input())
+arr = []
 
-word_list = []
 
-for case in range(N):
-    word = str(input())
+for i in range(n):
+    word = input()
     word_cnt = len(word)
-    word_list.append((word, word_cnt))
+    arr.append((word, word_cnt))
 
-word_list = list(set(word_list))
+arr = list(set(arr))
 
-word_list.sort(key = lambda word: (word[1], word[0]))
+arr.sort(key = lambda x : (x[1], x[0]))
 
-for i in word_list:
+for i in arr:
     print(i[0])
-
-
-
-# lambda 이해하고 다시 풀어보기
