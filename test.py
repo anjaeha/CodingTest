5e812fe4417ca6bc93d9458a6736d751c636051c
anjaeha <vix849@gmail.com>
2021-03-12 17:04:33 +0900

    2021.03.12

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-def solution(n):
-    answer = ''
-    
-    while n > 0:
-        answer += str(n%3)
-        n //= 3
-        
-    
-    return int(answer, 3)
+a1 = [[1,2],[2,3]]
 
-print(solution(45))
+print(len(a1))
