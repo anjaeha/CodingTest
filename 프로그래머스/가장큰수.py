6baff7c6bf565b1caaa3eb4549d1b0b70f290301
anjaeha <vix849@gmail.com>
2021-03-17 11:29:19 +0900

    파일 삭제

--- 프로그래머스/가장큰수.py
+++
@@ -1,7 +0,0 @@
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key = lambda x : x * 3, reverse=True)
-    if sum(list(map(int, numbers))) == 0:
-        numbers = list(set(numbers))
-        
-    return "".join(numbers)
