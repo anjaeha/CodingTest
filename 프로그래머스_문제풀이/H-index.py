2531f84ba05cfd7559a0a542fe016a267fc948dd
anjaeha <vix849@gmail.com>
2021-03-17 11:41:27 +0900

    프로그래머스 문제풀이

---
+++ 프로그래머스_문제풀이/H-index.py
@@ -0,0 +1,12 @@
+def solution(citations):
+    citations.sort()
+    answer = 0
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l - i:
+            return l - i
+        
+    return 0
+
+
+print(solution([3,0,6,1,5]))
