f87b2e9a1e3c39a22e28454a4c20fc2805234045
anjaeha <vix849@gmail.com>
2021-04-14 17:03:31 +0900

    2021.04.14

--- 백준_문제풀이/2174.py
+++ 백준_문제풀이/2174.py
@@ -3,16 +3,18 @@ input = sys.stdin.readline
 a, b = map(int, input().split())
 n, m = map(int, input().split())
 
-D = {'S' : 0, 'E' : 1, 'N' : 2, 'W' : 3}
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
 w = [[0] * (a+1) for _ in range(b+1)]
 r = [[0, 0, 0] for _ in range(n+1)]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+#상, 우, 하, 좌
+D = {'S' : 0, 'E' : 1, 'N' : 2, 'W' : 3}
+
 
 def solve(i, d, c):
-    x, y, z, = r[i]
+    x, y, z = r[i]
     w[x][y] = 0
+
     for _ in range(c):
         if d == 'L':
             z = (z+1) % 4
@@ -25,7 +27,7 @@ def solve(i, d, c):
                 print("Robot %d crashes into the wall" %i)
                 return True
             if w[x][y]:
-                print("Robot %d crashes into robot %d" % (i, w[x][y]))
+                print("Robot %d crashes into robot %d" %(i, w[x][y]))
                 return True
 
     r[i] = x, y, z
@@ -37,9 +39,10 @@ for i in range(1, n+1):
     x, y, z = input().split()
     w[int(y)][int(x)] = i
     r[i] = [int(y), int(x), D[z]]
+
 crash = False
 
-for i in range(m):
+for _ in range(m):
     i, d, c = input().split()
     if not crash:
         crash = solve(int(i), d, int(c))
