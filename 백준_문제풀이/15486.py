a6880935299b5ed2de1b66741def797131eae2da
anjaeha <vix849@gmail.com>
2021-06-09 13:01:45 +0900

    2021.06.09

--- 백준_문제풀이/15486.py
+++ 백준_문제풀이/15486.py
@@ -2,15 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-t = []
 p = []
+t = []
 dp = []
 
 for i in range(n):
-    a, b = map(int, input().split())
-    t.append(a)
-    p.append(b)
-    dp.append(b)
+    x, y = map(int, input().split())
+    t.append(x)
+    p.append(y)
+    dp.append(y)
 
 dp.append(0)
 
@@ -18,6 +18,6 @@ for i in range(n-1, -1, -1):
     if i + t[i] > n:
         dp[i] = dp[i+1]
     else:
-        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+        dp[i] = max(dp[i+1], p[i] + dp[i + t[i]])
 
 print(dp[0])
