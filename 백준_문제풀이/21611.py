b04f9fdea6adac5f0c3306a5886c0a43227aca9b
anjaeha <vix849@gmail.com>
2021-10-20 21:37:35 +0900

    2021.10.20

--- 백준_문제풀이/21611.py
+++ 백준_문제풀이/21611.py
@@ -1,118 +1,122 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
-dx = [0, -1, 1, 0, 0]
-dy = [0, 0, 0, -1, 1]
-
-sdx = [0, 1, 0, -1]
-sdy = [-1, 0, 1, 0]
-
-result = 0
-
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-move = [list(map(int, input().split())) for _ in range(m)]
-
-mid_x, mid_y = n // 2, n // 2
-graph[mid_x][mid_y] = 9
-ans1, ans2, ans3 = 0, 0, 0
-def move_snail(array):
+def blizard(d, s):
     x, y = n // 2, n // 2
-    for i in range(n * 2 - 1):
-        for k in range(i // 2 + 1):
-            try:
-                nx = x + sdx[i % 4]
-                ny = y + sdy[i % 4]
-                if 0 <= nx < n and 0 <= ny < n:
-                    graph[nx][ny] = array.pop(0)
-                    x, y = nx, ny
-            except:
-                break
-
-def boom(dir, dis):
-    x, y = n // 2, n // 2
-    for i in range(dis):
-        nx = x + dx[dir]
-        ny = y + dy[dir]
+    for i in range(s):
+        nx = x + dx[d]
+        ny = y + dy[d]
         graph[nx][ny] = 0
         x, y = nx, ny
 
-def search():
-    global ans1, ans2, ans3
-    global result
-    arr = []
-    x, y = n // 2, n // 2
-    for i in range(n * 2 - 1):
-        for k in range(i // 2 + 1):
-            nx = x + sdx[i % 4] 
-            ny = y + sdy[i % 4]
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] != 0:
-                    arr.append(graph[nx][ny])
-                x, y = nx, ny
+def ball_exp():
+    global sd, sx, sy, graph, ball
+    q = deque()
+    x, y = sx, sy
+    sd = 0
+    for case in range(2 * n - 1):
+        if case == 2 * n - 2:
+            time = case // 2
+        else:
+            time = case // 2 + 1
+
+        for i in range(time):
+            x = x + qdx[sd]
+            y = y + qdy[sd]
+
+            if graph[x][y] != 0:
+                q.append(graph[x][y])
+        sd = (sd + 1) % 4
+    if not q:
+        return
 
     while 1:
         flag = False
-        number = 0
-        count = []
-        color = []
-        for i in range(len(arr)):
-            if number == arr[i]:
-                count.append(count.pop() + 1)
+        que = deque()
+        temp = []
+        for i in range(len(q)):
+            if temp == []:
+                temp.append(q[i])
             else:
-                number = arr[i]
-                count.append(1)
-                color.append(number)
-
-        cnt = 0
-        for i in range(len(count)):
-            if count[i] >= 4:
-                flag = True
-                array = arr[:cnt] + arr[cnt+count[i]:]
-                idx = count[i]
-                boomColor = color[i]
-
-                if boomColor == 1:
-                    result += count[i]
-                    ans1 += count[i]
-                elif boomColor == 2:
-                    result += count[i] * 2
-                    ans2 += count[i]
-                elif boomColor == 3:
-                    result += count[i] * 3
-                    ans3 += count[i]
-                arr = array
-            else:
-                cnt += count[i]
+                if temp[0] == q[i]:
+                    temp.append(q[i])
+                else:
+                    if len(temp) >= 4:
+                        ball[temp[0]] += len(temp)
+                        temp = [q[i]]
+                        flag = True
+                    else:
+                        que.extend(temp)
+                        temp = [q[i]]
+        if len(temp) < 4:
+            que.extend(temp)
+        else:
+            ball[temp[0]] += len(temp)
 
         if not flag:
             break
-    
-    count = []
-    color = []
-    answer = []
-    number = 0
-    for i in range(len(arr)):
-        if number == arr[i]:
-            count.append(count.pop() + 1)
+        q = que
+
+    temp = []
+    que = deque()
+    for i in range(len(q)):
+        if temp == []:
+            temp.append(q[i])
         else:
-            number = arr[i]
-            count.append(1)
-            color.append(number)
+            if temp[-1] == q[i]:
+                temp.append(q[i])
+            else:
+                que.append(len(temp))
+                que.append(temp[0])
+                temp = [q[i]]
+    que.append(len(temp))
+    que.append(temp[0])
 
-    for i in range(len(count)):
-        answer.append(count[i])
-        answer.append(color[i])
+    board = [[0] * n for _ in range(n)]
+    x, y = sx, sy
+    sd = 0
+    for case in range(2 * n - 1):
+        if not que:
+            continue
+        if case == 2 * n - 2:
+            time = case // 2
+        else:
+            time = case // 2 + 1
 
-    return answer
+        for i in range(time):
+            x = x + qdx[sd]
+            y = y + qdy[sd]
 
-for case in range(m):
-    dir, dis = move[case]
-    boom(dir, dis)
-    q = search()
-    graph  = [[0] * n for _ in range(n)]
-    move_snail(q)
-    graph[n // 2][n // 2] = 9
+            if que:
+                board[x][y] = que.popleft()
+            else:
+                break
+        sd = (sd + 1) % 4
+    graph = board
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+exp = []
+for _ in range(m):
+    d, s = map(int, input().split())
+    exp.append((d - 1, s))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+qdx = [0, 1, 0, -1]
+qdy = [-1, 0, 1, 0]
+
+sx, sy = n // 2, n // 2 # 상어의 위치
+ball = [0, 0, 0, 0]
+
+for t in range(m):
+    dir, dis = exp[t]
+    blizard(dir, dis)
+    ball_exp()
+
+result = 0
+for i in range(1, 4):
+    result += ball[i] * i
 
 print(result)
