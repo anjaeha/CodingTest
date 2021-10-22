12a219e219e45c6c80558f94e193132127fd5e94
anjaeha <vix849@gmail.com>
2021-10-22 19:36:55 +0900

    2021.10.22

--- SWEA/벌꿀채취.py
+++ SWEA/벌꿀채취.py
@@ -1,70 +1,64 @@
-def make_candi(x, y): # 가능한 경우의 수 구하기
-    answer = []
-    if y + m > n:
-        return
-    
+# N * N크기의 벌통
+# 두 명의 일꾼은 M개가 가로로 연속되도록 선택하여 벌을 채취함.
+def make_candi(x, y):
     for i in range(0, y + m):
         visit[x][i] = True
-    
+    a = []
+    for i in range(m):
+        a.append(graph[x][y + i])
+
+    b = []
     for i in range(x, n):
         for j in range(n):
+            if visit[i][j] or j + m > n:
+                continue
             temp = []
-            if j + m > n:
-                break
             for k in range(m):
-                if not visit[i][j + k]:
-                    temp.append(graph[i][j + k])
-                else:
-                    break
-            if len(temp) == m:
-                answer.append(temp)
-    answer_xy = []
-    for i in range(m):
-        answer_xy.append(graph[x][y + i])
-    return answer_xy, answer # (answer_xy)일때 가능한 좌표 answer
+                temp.append(graph[i][j + k])
+            b.append(list(temp))
+    return a, b
 
-def honey_count(arr): # 꿀로 얻을 수 있는 최대의 수익 구하기
+def honey_count(arr):
     arr.sort()
-    answer = -1
-    for cnt in range(m): # 가장 큰 수 하나보다 다른 수의 합이 더 클 수 있음.
+    answer = 0
+    for i in range(m):
+        avg = c
         temp = 0
-        avb = c
-        for i in range(len(arr) - cnt - 1, -1, -1):
-            if arr[i] <= avb:
-                temp += arr[i] ** 2
-                avb -= arr[i]
-        answer = max(answer, temp)
+        for j in range(m - 1 - i, - 1, -1):
+            if arr[j] <= avg:
+                avg -= arr[j]
+                temp += arr[j] ** 2
+        answer = max(temp, answer)
     return answer
+
 
 T = int(input())
 
-for case in range(T):
+for tc in range(1, T + 1):
     n, m, c = map(int, input().split())
     graph = [list(map(int, input().split())) for _ in range(n)]
+    candi_A = []
+    candi_B = []
     result = -1
-    candiA = []
-    candiB = []
-    
-    # A의 위치가 정해졌을 때, B의 경우의 수를 구함.
+
     for i in range(n):
         for j in range(n):
             visit = [[False] * n for _ in range(n)]
             if j + m > n:
                 continue
-            temp_a, temp_b = make_candi(i, j)
-            if temp_a and temp_b:
-                candiA.append(temp_a)
-                candiB.append(temp_b)
-    
-    for idx in range(len(candiA)):
-        this_a = candiA[idx]
-        this_b = candiB[idx]
+            tempa, tempb = make_candi(i, j)
+            if tempa and tempb:
+                candi_A.append(tempa)
+                candi_B.append(tempb)
 
-        max_a = honey_count(this_a)
-        max_b = -1
-        for i in range(len(this_b)):
-            temp = honey_count(this_b[i])
-            max_b = max(temp, max_b)
-        
-        result = max(result, max_a + max_b)        
-    print("#%d %d" %(case + 1, result))
+    for i in range(len(candi_A)):
+        for j in range(len(candi_B[i])):
+            before = candi_A[i]
+            after = candi_B[i][j]
+
+            max_b = honey_count(before)
+            max_a = honey_count(after)
+
+            result = max(result, max_a + max_b)
+
+    print("#%d %d" %(tc, result))
--- SWEA/요리사.py
+++ SWEA/요리사.py
@@ -1,5 +1,4 @@
 def make_candi(depth):
-    global temp, candi
     if depth == n // 2:
         candi.append(list(temp))
         return
@@ -14,74 +13,41 @@ def make_candi(depth):
         for j in range(i + 1, n):
             visit[j] = False
 
-def make_1(depth, arr):
-    global temp
-    if depth == 2:
-        food1_result.append(list(temp))
-        return
-
-    for i in range(n // 2):
-        if visit[i]:
-            continue
-        visit[i] = True
-        temp.append(arr[i])
-        make_1(depth + 1, arr)
-        temp.pop()
-        for j in range(i + 1, n // 2):
-            visit[j] = False
-
-def make_2(depth, arr):
-    global temp
-    if depth == 2:
-        food2_result.append(list(temp))
-        return
-
-    for i in range(n // 2):
-        if visit[i]:
-            continue
-        visit[i] = True
-        temp.append(arr[i])
-        make_2(depth + 1, arr)
-        temp.pop()
-        for j in range(i + 1, n // 2):
-            visit[j] = False
-
 T = int(input())
 
 for tc in range(1, T + 1):
     n = int(input())
     graph = [list(map(int, input().split())) for _ in range(n)]
-    candi = []
+    result = 987654321
+
+    length = n // 2
     temp = []
+    candi = []
     visit = [False] * n
     make_candi(0)
-    result = 10000000
-    for i in range(len(candi)):
-        food1 = candi[i]
-        food2 = []
-        for j in range(n):
-            if j not in food1:
-                food2.append(j)
-        food1_result = []
-        food2_result = []
-        visit = [False] * (n // 2)
-        temp = []
-        make_1(0, food1)
-        
-        visit = [False] * (n // 2)
-        temp = []
-        make_2(0, food2)
 
-        one_point = 0
-        for x, y in food1_result:
-            one_point += graph[x][y]
-            one_point += graph[y][x]
-        
-        two_point = 0
-        for x, y in food2_result:
-            two_point += graph[x][y]
-            two_point += graph[y][x]
-        
-        result = min(result, abs(one_point - two_point))
-    
+    for i in range(len(candi)):
+        cur = candi[i]
+        other = []
+        for num in range(n):
+            if num not in cur:
+                other.append(num)
+
+        size1 = 0
+        size2 = 0
+        for a in range(len(cur)):
+            for b in range(a + 1, len(cur)):
+                x = cur[a]
+                y = cur[b]
+                size1 += graph[x][y]
+                size1 += graph[y][x]
+        for a in range(len(other)):
+            for b in range(a + 1, len(other)):
+                x = other[a]
+                y = other[b]
+                size2 += graph[x][y]
+                size2 += graph[y][x]
+
+        result = min(result, abs(size1 - size2))
+
     print("#%d %d" %(tc, result))
