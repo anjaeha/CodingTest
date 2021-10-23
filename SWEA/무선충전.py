1dbbfff2dde79bb84564d35aaafd41dd19fcc3c1
anjaeha <vix849@gmail.com>
2021-10-23 20:29:14 +0900

    2021.10.23

--- SWEA/무선충전.py
+++ SWEA/무선충전.py
@@ -1,48 +1,53 @@
-T = int(input())
+# 10 * 10크기의 그래프, 사용자는 2명, (0,0) (9,9)에서 시작함.
 
 dx = [0, -1, 0, 1, 0]
 dy = [0, 0, 1, 0, -1]
 
+T = int(input())
 for tc in range(1, T + 1):
-    m, a = map(int, input().split())
+    n, a = map(int, input().split())
     move_a = list(map(int, input().split()))
     move_b = list(map(int, input().split()))
+    graph = [[[(0, 0)] for _ in range(10)] for _ in range(10)]
 
-    graph = [[[(0,0)], [(0,0)],[(0,0)],[(0,0)],[(0,0)],[(0,0)],[(0,0)],[(0,0)],[(0,0)],[(0,0)]] for i in range(10)]
-
-    for d in range(a):
-        y, x, c, p = map(int, input().split())
+    for case in range(a):
+        y, x, c, p = map(int, input().split()) # (x, y)에 충전범위, 성능
         x, y = x - 1, y - 1
         for i in range(10):
             for j in range(10):
-                if abs(x - i) + abs(y - j) <= c:
-                    graph[i][j] += [(d + 1, p)]
-
+                if abs(i - x) + abs(j - y) <= c:
+                    if graph[i][j] == 0:
+                        graph[i][j] = [case + 1, p]
+                    else:
+                        graph[i][j].append((case + 1, p))
     ax, ay = 0, 0
+    result_a = [graph[ax][ay]]
+    for i in range(n):
+        ax = ax + dx[move_a[i]]
+        ay = ay + dy[move_a[i]]
+        result_a.append(graph[ax][ay])
+
     bx, by = 9, 9
+    result_b = [graph[bx][by]]
+    for i in range(n):
+        bx = bx + dx[move_b[i]]
+        by = by + dy[move_b[i]]
+        result_b.append(graph[bx][by])
+
     result = 0
+    for i in range(n + 1):
+        cura = result_a[i]
+        curb = result_b[i]
+        cura.sort(key = lambda x: -x[1])
+        curb.sort(key = lambda x: -x[1])
 
-    for case in range(m + 1):
-        order = sorted(graph[ax][ay], key = lambda x: -x[1]) # 현재 위치에서 충전 가능한 곳을 내림차순으로 받음
-        order2 = sorted(graph[bx][by], key = lambda x: -x[1])
-        a_max = order[0] # 현 위치에서 충전 최대치를 받아냄
-        b_max = order2[0]
+        if len(cura) == len(curb) == 1:
+            continue
 
-        if a_max[0] == b_max[0] and a_max[0] != 0: # 현 위치에서 충전 최대치가 같은 BC면
-            a_sem = order[1] # 그 다음값을 받아냄
-            b_sem = order2[1]
-
-            if a_sem == b_sem == (0, 0): # 현 위치에 충전기가 하나라면 값을 더해주고
-                result += a_max[1]
-            else:
-                result += (a_max[1] + max(a_sem[1], b_sem[1])) # 아니라면 최대값 + 그 다음 최대값값
+        if cura[0][0] == curb[0][0]:
+            result += cura[0][1]
+            result += max(cura[1][1], curb[1][1])
         else:
-            result += a_max[1] + b_max[1] # 같지않으면 두 최대값을 더해줌줌
-
-
-        if case == m:
-            break
-        ax, ay = ax + dx[move_a[case]], ay + dy[move_a[case]] # 다음 위치로 이동
-        bx, by = bx + dx[move_b[case]], by + dy[move_b[case]]
+            result += cura[0][1] + curb[0][1]
 
     print("#%d %d" %(tc, result))
--- SWEA/핀볼게임.py
+++ SWEA/핀볼게임.py
@@ -1,77 +1,65 @@
-# 1 ~ 5 블록, 6 ~ 10 웜홀, -1 블랙홀
-# 출발위치와 방향 선택하여 최대값 구하기
-# 빈 공간에서 시작해야함.
-
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-# 상, 하, 좌, 우
+
 def move(sx, sy, d):
     x, y = sx, sy
-    answer = 0
+    count = 0
 
     while 1:
         nx = x + dx[d]
         ny = y + dy[d]
 
-        if nx == -1 or ny == -1 or nx == n or ny == n:
-            return (answer * 2 + 1)
+        if nx < 0 or ny < 0 or nx >= n or ny >= n:
+            return (count * 2 + 1)
 
-        if nx == sx and ny == sy:
-            return answer
-        
+        if graph[nx][ny] == -1 or (nx == sx and ny == sy):
+            return count
+
         if graph[nx][ny] == 0:
             x, y = nx, ny
             continue
         elif graph[nx][ny] == 1:
-            answer += 1
-            if d == 0:
-                d ^= 1
-            elif d == 1:
+            count += 1
+            if d == 1:
                 d = 3
             elif d == 2:
                 d = 0
-            elif d == 3:
+            else:
                 d ^= 1
         elif graph[nx][ny] == 2:
-            answer += 1
+            count += 1
             if d == 0:
                 d = 3
-            elif d == 1:
-                d ^= 1
             elif d == 2:
                 d = 1
-            elif d == 3:
+            else:
                 d ^= 1
         elif graph[nx][ny] == 3:
-            answer += 1
+            count += 1
             if d == 0:
                 d = 2
-            elif d == 1:
-                d ^= 1
-            elif d == 2:
-                d ^= 1
             elif d == 3:
                 d = 1
+            else:
+                d ^= 1
         elif graph[nx][ny] == 4:
-            answer += 1
-            if d == 0:
-                d ^= 1
-            elif d == 1:
+            count += 1
+            if d == 1:
                 d = 2
-            elif d == 2:
-                d ^= 1
             elif d == 3:
                 d = 0
+            else:
+                d ^= 1
         elif graph[nx][ny] == 5:
-            answer += 1
+            count += 1
             d ^= 1
         elif 6 <= graph[nx][ny] <= 10:
-            for p in portal[graph[nx][ny]]:
-                if p != [nx, ny]:
-                    nx, ny = p[0], p[1]
-                    break
-        elif graph[nx][ny] == -1:
-            return answer
+            for rc in holl[graph[nx][ny]]:
+                if nx == rc[0] and ny == rc[1]:
+                    continue
+                nx = rc[0]
+                ny = rc[1]
+                break
 
         x, y = nx, ny
 
@@ -80,23 +68,23 @@ T = int(input())
 for tc in range(1, T + 1):
     n = int(input())
     graph = [list(map(int, input().split())) for _ in range(n)]
-
-    result = -1
+    holl = {}
     zeros = []
-    portal = {}
     for i in range(n):
         for j in range(n):
             if graph[i][j] == 0:
                 zeros.append((i, j))
             elif 6 <= graph[i][j] <= 10:
-                if portal.get(graph[i][j]) is None:
-                    portal[graph[i][j]] = [[i, j]]
+                if holl.get(graph[i][j]) is None:
+                    holl[graph[i][j]] = [(i, j)]
                 else:
-                    portal[graph[i][j]].append([i, j])
+                    holl[graph[i][j]].append((i, j))
 
-
+    result = 0
     for x, y in zeros:
         for d in range(4):
             temp = move(x, y, d)
             result = max(result, temp)
+
+
     print("#%d %d" %(tc, result))
