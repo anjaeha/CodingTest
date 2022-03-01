d8c7a293b5f033f0914736962257d8f08eba1247
anjaeha <vix849@gmail.com>
2022-03-01 11:55:21 +0900

    2022.03.01

--- 백준_문제풀이/11660.py
+++ 백준_문제풀이/11660.py
@@ -1,21 +1,18 @@
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
+graph = [[0] * (n + 1)]
+for _ in range(n):
+    temp = [0]
+    temp += list(map(int, input().split()))
+    graph.append(temp)
+point = [list(map(int, input().split())) for _ in range(m)]
 
-s = []
-for i in range(n):
-    s.append(list(map(int, input().split())))
 
-dp = [[0] * (n+1) for _ in range(n+1)]
-
-for i in range(n):
-    for j in range(n):
-        dp[i+1][j+1] = dp[i+1][j] + dp[i][j+1] + s[i][j] - dp[i][j]
+board = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        board[i][j] = board[i - 1][j] + board[i][j - 1] - board[i - 1][j - 1] + graph[i][j]
 
 for i in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
-
-    result = dp[x2][y2] - (dp[x2][y1-1] + dp[x1-1][y2] - dp[x1-1][y1-1])
-
+    x1, y1, x2, y2 = point[i]
+    result = board[x2][y2] - (board[x2][y1 - 1] + board[x1 - 1][y2] - board[x1 - 1][y1 - 1])
     print(result)
--- 백준_문제풀이/2638.py
+++ 백준_문제풀이/2638.py
@@ -1,24 +1,16 @@
 from collections import deque
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def melt(): # 다 녹았는지를 검사하기 위한 함수
-    SUM = 0
+def check():
     for i in range(n):
-        SUM += sum(graph[i])
-    return SUM == 0
+        for j in range(m):
+            if graph[i][j] == 1:
+                return False
+    return True
 
-time = 0
-while 1:
-    if melt():
-        break
-    time += 1
+def outside():
     q = deque()
     q.append((0, 0))
-    board = [[0] * m for _ in range(n)]
+    graph[0][0] = 2
     while q:
         x, y = q.popleft()
         for d in range(4):
@@ -26,25 +18,34 @@ while 1:
             ny = y + dy[d]
 
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 0 and board[nx][ny] == 0:
-                    board[nx][ny] = 2 # 외부공기는 2로
+                if graph[nx][ny] == 0:
                     q.append((nx, ny))
-                elif graph[nx][ny] == 1 and board[nx][ny] == 0:
-                    board[nx][ny] = 1 # 치즈는 1
+                    graph[nx][ny] = 2  # 외부공기2
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-    for x in range(n):
-        for y in range(m):
-            if board[x][y] == 1:
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)] # 0빈공간, 1치즈
+
+count = 0
+while 1:
+    if check():
+        print(count)
+        break
+    count += 1
+    board = [i[:] for i in graph]
+    outside()
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
                 cnt = 0
                 for d in range(4):
-                    nx = x + dx[d]
-                    ny = y + dy[d]
+                    nx = i + dx[d]
+                    ny = j + dy[d]
                     if 0 <= nx < n and 0 <= ny < m:
-                        if board[nx][ny] == 2:
+                        if graph[nx][ny] == 2:
                             cnt += 1
                 if cnt >= 2:
-                    graph[x][y] = 0
-                else:
-                    graph[x][y] = 1
-print(time)
+                    board[i][j] = 0
+    graph = board
