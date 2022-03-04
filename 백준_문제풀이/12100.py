39ec6ccdb21b05f91de0bb4d10b73f44f2bd1dee
anjaeha <vix849@gmail.com>
2022-03-04 11:44:27 +0900

    2022.03.04

--- 백준_문제풀이/12100.py
+++ 백준_문제풀이/12100.py
@@ -1,16 +1,13 @@
 from collections import deque
-from copy import deepcopy
-
-def move(dir):
-    # 위로
-    if dir == 0:
+def move(d):
+    if d == 0: # 위로 올렸을때
         q = deque()
         for i in range(n):
             for j in range(n):
                 if graph[j][i]:
                     q.append(graph[j][i])
                     graph[j][i] = 0
-            answer = []
+            answer = deque()
             while q:
                 temp = q.popleft()
                 if q:
@@ -23,19 +20,17 @@ def move(dir):
                     answer.append(temp)
             for j in range(n):
                 if answer:
-                    graph[j][i] = answer.pop(0)
+                    graph[j][i] = answer.popleft()
                 else:
                     graph[j][i] = 0
-
-    # 아래로             
-    elif dir == 1:
+    elif d == 1: # 아래로 내렸을 때
         q = deque()
         for i in range(n):
             for j in range(n - 1, -1, -1):
                 if graph[j][i]:
                     q.append(graph[j][i])
                     graph[j][i] = 0
-            answer = []
+            answer = deque()
             while q:
                 temp = q.popleft()
                 if q:
@@ -48,18 +43,17 @@ def move(dir):
                     answer.append(temp)
             for j in range(n - 1, -1, -1):
                 if answer:
-                    graph[j][i] = answer.pop(0)
+                    graph[j][i] = answer.popleft()
                 else:
                     graph[j][i] = 0
-    # 왼쪽으로
-    elif dir == 2:
+    elif d == 2: # 왼쪽으로 밀었을때
         q = deque()
         for i in range(n):
             for j in range(n):
                 if graph[i][j]:
                     q.append(graph[i][j])
                     graph[i][j] = 0
-            answer = []
+            answer = deque()
             while q:
                 temp = q.popleft()
                 if q:
@@ -72,19 +66,17 @@ def move(dir):
                     answer.append(temp)
             for j in range(n):
                 if answer:
-                    graph[i][j] = answer.pop(0)
+                    graph[i][j] = answer.popleft()
                 else:
                     graph[i][j] = 0
-
-    # 오른쪽으로
-    elif dir == 3:
+    elif d == 3: # 왼쪽으로 밀었을때
         q = deque()
         for i in range(n):
             for j in range(n - 1, -1, -1):
                 if graph[i][j]:
                     q.append(graph[i][j])
                     graph[i][j] = 0
-            answer = []
+            answer = deque()
             while q:
                 temp = q.popleft()
                 if q:
@@ -97,31 +89,26 @@ def move(dir):
                     answer.append(temp)
             for j in range(n - 1, -1, -1):
                 if answer:
-                    graph[i][j] = answer.pop(0)
+                    graph[i][j] = answer.popleft()
                 else:
                     graph[i][j] = 0
 
-
-# 최대 5번 이동시켜서 얻을 수 있는 가장 큰 블록
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-
-result = -1
+MAX = -1
 def dfs(depth):
-    global result, graph
+    global MAX, graph
     if depth == 5:
         for i in range(n):
-            for j in range(n):
-                if graph[i][j] > result:
-                    result = graph[i][j]
+            MAX = max(MAX, max(graph[i]))
         return
 
-    temp = deepcopy(graph)
-    for i in range(4):
-        move(i)
+    board = [item[:] for item in graph]
+    for d in range(4):
+        move(d)
         dfs(depth + 1)
-        graph = deepcopy(temp)
+        graph = [item[:] for item in board]
 
 dfs(0)
-print(result)
+print(MAX)
--- 백준_문제풀이/13460.py
+++ 백준_문제풀이/13460.py
@@ -7,51 +7,47 @@ n, m = map(int, input().split())
 graph = [list(input()) for _ in range(n)]
 visit = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
 
-q = deque()
-
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 'R':
             rx, ry = i, j
         elif graph[i][j] == 'B':
             bx, by = i, j
+q = deque()
 q.append((rx, ry, bx, by, 1))
 visit[rx][ry][bx][by] = True
 
 def move(x, y, d):
     cnt = 0
     while graph[x + dx[d]][y + dy[d]] != '#' and graph[x][y] != 'O':
-        x += dx[d]
-        y += dy[d]
+        x = x + dx[d]
+        y = y + dy[d]
         cnt += 1
-
     return x, y, cnt
-    
+
 
 def bfs():
     while q:
-        rx, ry, bx, by, idx = q.popleft()
-        if idx > 10:
-            break
+        rx, ry, bx, by, cnt = q.popleft()
+        for d in range(4):
+            rnx, rny, rcnt = move(rx, ry, d)
+            bnx, bny, bcnt = move(bx, by, d)
 
-        for i in range(4):
-            nrx, nry, rcnt = move(rx, ry, i)
-            nbx, nby, bcnt = move(bx, by, i)
+            if graph[bnx][bny] != 'O': # 파란색이 나가지 않을때만 진행
+                if graph[rnx][rny] == 'O': # 빨간색이 나가면 종료
+                    return cnt
 
-            if graph[nbx][nby] != 'O':
-                if graph[nrx][nry] == 'O':
-                    print(idx)
-                    return
-                if nrx == nbx and nry == nby:
-                    if rcnt > bcnt:
-                        nrx -= dx[i]
-                        nry -= dy[i]
+                if (rnx, rny) == (bnx, bny): # 만약 좌표가 같으면, 더 움직인것(멀리서온것)을 하나 뒤로 빼줌
+                    if rcnt < bcnt: # 파란색이 더 멀리서 옴
+                        bnx, bny = bnx - dx[d], bny - dy[d]
                     else:
-                        nbx -= dx[i]
-                        nby -= dy[i]
-                if visit[nrx][nry][nbx][nby] == False:
-                    visit[nrx][nry][nbx][nby] = True
-                    q.append((nrx, nry, nbx, nby, idx + 1))
-    
-    print(-1)
-bfs()
+                        rnx, rny = rnx - dx[d], rny - dy[d]
+
+                if not visit[rnx][rny][bnx][bny]:
+                    visit[rnx][rny][bnx][bny] = True
+                    if cnt < 10:
+                        q.append((rnx, rny, bnx, bny, cnt + 1))
+    return -1
+
+answer = bfs()
+print(answer)
