3d6b5cd79e36ee6e1e5e1bbd90241e8f6feb519a
anjaeha <vix849@gmail.com>
2022-03-10 11:18:54 +0900

    2022.03.10

--- 백준_문제풀이/15683.py
+++ 백준_문제풀이/15683.py
@@ -1,40 +1,24 @@
-from copy import deepcopy
-n, m = map(int, input().split()) # 사무실의 크기
-graph = [list(map(int, input().split())) for _ in range(n)] # 0은 빈칸, 6은 벽, 1~ 5는 CCTV
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-MIN = 100 # 사각지대의 개수
-def check():
-    cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                cnt += 1
-    return cnt
+# 상, 우, 하, 좌
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-# 상, 하, 좌, 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-direct = [[], [[0], [1], [2], [3]], [[0, 1], [2, 3]], [[0, 3], [0, 2], [1, 3], [1, 2]], [[0, 2, 3], [1, 2, 3], [0, 1, 3], [0, 1, 2]], [[0, 1, 2, 3]]]
-# 1번은 상 / 하 / 좌 / 우
-# 2번은 상, 하 / 좌, 우
-# 3번은 상, 우 / 상, 좌 / 하, 우 / 하, 좌
-# 4번은 상, 좌, 우 / 하, 좌, 우 / 상 하, 우 / 상, 하, 좌
-# 5번은 상 하 좌 우 
+direct = [[], [[0], [1], [2], [3]], [[0, 2], [1, 3]], [[0, 1], [1, 2], [2, 3], [3, 0]], [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]], [[0, 1, 2, 3]]]
 
 camera = []
 for i in range(n):
     for j in range(m):
-        if 0 < graph[i][j] < 6:
+        if graph[i][j] in [1,2,3,4,5]:
             camera.append((i, j, graph[i][j]))
-
 
 def fill(x, y, d):
     for i in d:
         nx, ny = x, y
         while 1:
-            nx += dx[i]
-            ny += dy[i]
+            nx = nx + dx[i]
+            ny = ny + dy[i]
 
             if 0 <= nx < n and 0 <= ny < m:
                 if graph[nx][ny] != 6:
@@ -45,19 +29,29 @@ def fill(x, y, d):
             else:
                 break
 
+
+
+MIN = int(1e9)
 def dfs(depth):
     global MIN, graph
     if depth == len(camera):
-        temp = check()
-        MIN = min(MIN, temp)
+        MIN = min(MIN, check())
         return
 
-    temp_graph = deepcopy(graph)           
+    temp_graph = [i[:] for i in graph]
     x, y, dir = camera[depth]
     for i in direct[dir]:
         fill(x, y, i)
         dfs(depth + 1)
-        graph = deepcopy(temp_graph)
+        graph = [i[:] for i in temp_graph]
+
+def check():
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                cnt += 1
+    return cnt
 
 dfs(0)
 print(MIN)
--- 백준_문제풀이/15684.py
+++ 백준_문제풀이/15684.py
@@ -1,46 +1,42 @@
-n, m, h = map(int, input().split()) # 세로선의 개수, 가로선의 개수, 가로선을 놓을 수 있는 위치의 개수
-
-graph = [[False] * n for _ in range(h)]
+n, m, h = map(int, input().split()) # 세로선의 개수 N, 선의 개수 M, 가로선을 놓을 수 있는 위치 h
+graph = [[False] * n for _ in range(h)] # 세로의 길이H, 가로의 길이N
 
 for i in range(m):
     a, b = map(int, input().split())
     graph[a - 1][b - 1] = True
 
-# 사다리게임의 조건을 만족하는지
 def check():
-    for idx in range(n):
-        k = idx
-        for i in range(h):
-            if graph[i][k]:
-                k += 1
-            elif k > 0 and graph[i][k - 1]:
-                k -= 1
-        if idx != k:
+    for i in range(n):
+        pos = i
+        for j in range(h):
+            if graph[j][pos]:
+                pos += 1
+            elif pos > 0 and graph[j][pos - 1]:
+                pos -= 1
+        if pos != i:
             return False
     return True
-        
-# 모든 지역에서 사다리 추가해보기
+
 def dfs(depth, x, y):
     global result
+    if result <= depth:
+        return
+
     if check():
-        result = min(result, depth)
+        result = depth
         return
-    
-    if depth == 3 or result <= depth:
+
+    if depth == 3:
         return
-    
+
     for i in range(x, h):
-        if i == x:  # i가 x와 같으면 (x, y)부터 시작하여 사다리를 그어주고
-            k = y
-        else: # 아니라면 (x, 0)부터 사다리를 그어주는 방식이다.
-            k = 0
-        
-        for j in range(k, n - 1):
+        for j in range(y, n - 1):
             if not graph[i][j] and not graph[i][j + 1]:
                 graph[i][j] = True
-                dfs(depth + 1, i, j + 2)
+                dfs(depth + 1, i, j)
                 graph[i][j] = False
+        y = 0
 
 result = 4
 dfs(0, 0, 0)
-print(result if result != 4 else -1)
+print(result if result < 4 else -1)
