1e5b5983103780216eb7ba5da3787c4c5534b847
anjaeha <vix849@gmail.com>
2022-03-16 11:32:31 +0900

    2022.03.16

--- 백준_문제풀이/17140.py
+++ 백준_문제풀이/17140.py
@@ -7,71 +7,89 @@ r, c, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(3)]
 # graph[r][c] == k가 되는 최소 연산 시간 구하기 (100초 넘으면 - 1)
 
-def calC():
-    s = [[0] * len(graph) for _ in range(len(graph[0]))]
+def row_sort():
+    global graph
+    for idx in range(len(graph)):
+        count = {}
+        arr = graph[idx]
+        for j in arr:
+            if j == 0:
+                continue
+            if j in count:
+                count[j] += 1
+            else:
+                count[j] = 1
+
+        sort_arr = sorted(count.items(), key = lambda x : (x[1], x[0]))
+        arr = []
+        for rc, val in sort_arr:
+            arr.extend((rc, val))
+
+        graph[idx] = arr
+    long = -1
+    for idx in range(len(graph)):
+        long = max(long, len(graph[idx]))
+
+    for i in range(len(graph)):
+        for _ in range(long - len(graph[i])):
+            graph[i].append(0)
+
+def col_sort():
+    global graph
+    board = []
     for i in range(len(graph[0])):
+        arr = []
         for j in range(len(graph)):
-              s[i][j] = graph[j][i]
-    MAX = 0
-    for i in range(len(s)):
-        arr = s[i]
-        s[i] = []
-        temp = {}
+            arr.append(graph[j][i])
+        board.append(arr)
+
+    for idx in range(len(board)):
+        count = {}
+        arr = board[idx]
         for j in arr:
-            if j != 0:
-                if j in temp:
-                    temp[j] += 1
-                else:
-                    temp[j] = 1        
-        temp = sorted(temp.items(), key = lambda x : (x[1], x[0]))
-        for rc, val in temp:
-            s[i].append(rc)
-            s[i].append(val)
-        MAX = max(MAX, len(s[i]))
-    
-    for i in range(len(s)):
-        while len(s[i]) != MAX:
-            s[i].append(0)
+            if j == 0:
+                continue
+            if j in count:
+                count[j] += 1
+            else:
+                count[j] = 1
 
-    answer = [[0] * len(s) for _ in range(len(s[0]))]
-    for i in range(len(s)):
-        for j in range(len(s[0])):
-            answer[j][i] = s[i][j]
-    return answer
+        sort_arr = sorted(count.items(), key = lambda x : (x[1], x[0]))
+        arr = []
+        for rc, val in sort_arr:
+            arr.extend((rc, val))
 
-def calR():
-    s = [[] for _ in range(len(graph))]
-    MAX = 0
-    for i in range(len(s)):
-        arr = graph[i]
-        temp = {}
-        for j in arr:
-            if j != 0:
-                if j in temp:
-                    temp[j] += 1
-                else:
-                    temp[j] = 1        
-        temp = sorted(temp.items(), key = lambda x : (x[1], x[0]))
-        for rc, val in temp:
-            s[i].append(rc)
-            s[i].append(val)
-        MAX = max(MAX, len(s[i]))
-    
-    for i in range(len(s)):
-        while len(s[i]) != MAX:
-            s[i].append(0)
-    return s
+        board[idx] = arr
+    long = -1
+    for idx in range(len(board)):
+        long = max(long, len(board[idx]))
 
-cnt = 0
-while cnt <= 100:
-    if len(graph) >= r and len(graph[0]) >= c:
+    for i in range(len(board)):
+        for _ in range(long - len(board[i])):
+            board[i].append(0)
+
+    new_graph = []
+    for i in range(len(board[0])):
+        arr = []
+        for j in range(len(board)):
+            arr.append(board[j][i])
+        new_graph.append(arr)
+    graph = new_graph
+
+
+result = 0
+while 1:
+    if len(graph) > (r - 1) and len(graph[0]) > (c - 1):
         if graph[r - 1][c - 1] == k:
             break
+    if result > 100:
+        result = -1
+        break
     if len(graph) >= len(graph[0]):
-        graph = calR()
+        row_sort()
     else:
-        graph = calC()
+        col_sort()
 
-    cnt += 1
+    result += 1
 
-print(cnt if cnt != 101  else -1)
+print(result)
--- 백준_문제풀이/17142.py
+++ 백준_문제풀이/17142.py
@@ -1,100 +1,7 @@
-from collections import deque
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-# 바이러스의 위치를 넣어줌
-candi_virus = []
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 2:
-            candi_virus.append((i, j))
-        
-
-# 바이러스 확산
-def spread(arr):
-    distance = [[-1] * n for _ in range(n)]
-    visit = [[False] * n for _ in range(n)]
-    q = deque()
-    for i in range(len(arr)):
-        visit[arr[i][0]][arr[i][1]] = True
-        distance[arr[i][0]][arr[i][1]] = 0
-        q.append(arr[i])
-
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < n:
-                if visit[nx][ny] == False and graph[nx][ny] != 1:
-                    distance[nx][ny] = distance[x][y] + 1
-                    visit[nx][ny] = True
-                    q.append((nx, ny))
-
-
-    return distance
-
-# 바이러스가 안퍼지는 경우가 있는지 확인 (벽이거나 바이러스면 안퍼짐)
-def check(arr):
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] == -1:
-                if graph[i][j] == 1 or graph[i][j] == 2:
-                    continue
-                else:
-                    return False
-    return True
-
-
-
-
-# 백트래킹으로 바이러스를 선택할 수 있는 경우의수 탐색
-result = 10000
-use = [False] * len(candi_virus)
-virus = []
-def dfs(depth):
-    global result
-    if depth == m:
-        # 바이러스 후보중 m개가 선택되어있는 상황임.
-        temp = 0
-        vir = spread(virus)
-        if check(vir):
-            for i in range(n):
-                for j in range(n):
-                    if temp < vir[i][j] and graph[i][j] != 2:
-                        temp = vir[i][j]
-            if result > temp:
-                result = temp
-        return
-
-
-    
-    for i in range(len(candi_virus)):
-        if use[i]:
-            continue
-
-        use[i] = True
-        virus.append((candi_virus[i]))
-        dfs(depth + 1)
-        
-        virus.pop()
-        for j in range(i + 1, len(candi_virus)):
-            use[j] = False
-
-dfs(0)
-
-print(result if result != 10000 else -1)
-
-
-"""
-# 바이러스는 상하좌우로 동시에 복제
-# M개를 활성화
-# 모든 칸에 바이러스 퍼뜨리는데 걸리는 최소 시간 구하기
+# 바이러스는 상하좌우로 모든칸으로 동시복제
+# M개를 활성 상태로 변경
+# 모든 빈 칸에 바이러스를 퍼뜨리는 최소 시간
+# 빈칸 - 0, 벽 - 1, 바이러스 - 2
 from collections import deque
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -102,85 +9,78 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
-virus = []
+virus_pos = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 2:
-            virus.append((i, j))
-            graph[i][j] = '#'
+            virus_pos.append((i, j))
 
-
-
-def make_candi(depth): # M개의 바이러스를 선택하는 경우의 수
-    global temp
+candi = []
+visit = [False] * len(virus_pos)
+temp = []
+def make_candi(depth):
     if depth == m:
-        virus_pos.append(list(temp))
+        candi.append(list(temp))
         return
-    
-    for i in range(len(virus)):
+
+    for i in range(len(virus_pos)):
         if visit[i]:
             continue
         visit[i] = True
-        temp.append(i)
+        temp.append(virus_pos[i])
         make_candi(depth + 1)
         temp.pop()
-        for j in range(i + 1, len(virus)):
+        for j in range(i + 1, len(virus_pos)):
             visit[j] = False
-virus_pos = []
-temp = []
-visit = [False] * len(virus)
-make_candi(0)
 
+make_candi(0) # 활성화되는 경우의 수
 
-def check(s):
-    for i in range(n):
-        for j in range(n):
-            if s[i][j] == 0:
-                if (i, j) in  virus:
-                    continue
-                else:
-                    return False
-    return True
-
-def make_virus(arr):
+def spread(arr):
+    board = [[-1] * n for _ in range(n)]
     q = deque()
-    s = [item[:] for item in graph]
     visit = [[False] * n for _ in range(n)]
-    for i in range(len(arr)):
-        q.append(virus[arr[i]])
-        s[virus[arr[i]][0]][virus[arr[i]][1]] = 0
-        visit[virus[arr[i]][0]][virus[arr[i]][1]] = True
+    for x, y in arr:
+        q.append((x, y))
+        visit[x][y] = True
+        board[x][y] = 0
+
     while q:
         x, y = q.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
 
             if 0 <= nx < n and 0 <= ny < n:
-                if (s[nx][ny] == 0 or s[nx][ny] == '#') and visit[nx][ny] == False:
-                    s[nx][ny] = s[x][y] + 1
+                if graph[nx][ny] != 1 and not visit[nx][ny]: # 벽이 아니고 방문하지 않았을 때
+                    board[nx][ny] = board[x][y] + 1
                     visit[nx][ny] = True
                     q.append((nx, ny))
-    
-    sec = 0
-    flag = check(s)
-    if flag:
-        for i in range(n):
-            for j in range(n):
-                if graph[i][j] == 0:
-                    if s[i][j] > sec:
-                        sec = s[i][j]
-        return sec
-    return -2
 
-MIN = 1000000
-for case in range(len(virus_pos)):
-    arr = virus_pos[case]
-    result = make_virus(arr)
-    if result >= 0:
-        MIN = min(MIN, result)
+    # 몇초 걸렸는지 출력
+    result = -1
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] == 0:
+                if not visit[i][j]: # 벽이 아닌데, 방문하지 않았으면 False 출력 => 실패한 경우
+                    return False
+                result = max(result, board[i][j])
+    return result
 
-print(MIN if MIN != 1000000 else -1)
-"""
+MIN = int(1e9)
+
+for idx in range(len(candi)):
+    temp = spread(candi[idx])
+    if temp == False: # False값이 오면 패스
+        continue
+
+    if temp == -1:
+        MIN = 0
+        break
+    MIN = min(MIN, temp)
+    if MIN == 0:
+        break
+
+if MIN == int(1e9):
+    print(-1)
+else:
+    print(MIN)
