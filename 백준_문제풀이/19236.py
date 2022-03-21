af6156220a020a807c1b1259cb9a3975336e99f6
anjaeha <vix849@gmail.com>
2022-03-21 11:31:40 +0900

    2022.03.21

--- 백준_문제풀이/19236.py
+++ 백준_문제풀이/19236.py
@@ -1,30 +1,27 @@
 from copy import deepcopy
 
-# 물고기의 위치 찾기
-def find_fish(idx):
+def find_number(idx):  # 해당 번호의 위치 찾기
     for i in range(4):
         for j in range(4):
             if graph[i][j][0] == idx:
-                return (i, j)
+                return i, j
     return False
 
-# 작은 물고기부터 움직이기 시작
-def fish_move(shark_x, shark_y):
+def fish_move(sx, sy):
     for idx in range(1, 17):
-        position = find_fish(idx)
-        if position == False:
+        pos = find_number(idx)
+        if not pos:
             continue
-        x, y = position[0], position[1]
-        for _ in range(8):
+        x, y = pos
+        for i in range(8):
             d = graph[x][y][1]
-            nx, ny = x + dx[d], y + dy[d]
-            if 0 <= nx < 4 and 0 <= ny < 4:
-                if not (nx == shark_x and ny == shark_y):
-                    graph[x][y], graph[nx][ny] = graph[nx][ny], graph[x][y]
-                    break
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < 4 and 0 <= ny < 4 and (nx, ny) != (sx, sy):
+                graph[x][y], graph[nx][ny] = graph[nx][ny], graph[x][y]
+                break
             graph[x][y][1] = (graph[x][y][1] + 1) % 8
 
-# 상어가 갈 수 있는 위치 찾기
 def shark_food(x, y):
     food = []
     d = graph[x][y][1]
@@ -32,29 +29,26 @@ def shark_food(x, y):
         nx = x + dx[d] * i
         ny = y + dy[d] * i
 
-        if 0 <= nx < 4 and 0 <= ny < 4 and 1 <= graph[nx][ny][0] <= 16:
+        if 0 <= nx < 4 and 0 <= ny < 4 and graph[nx][ny][0] > 0:
             food.append((nx, ny))
     return food
 
-
-shark = 0
+result = 0
 def dfs(x, y, total):
-    global shark, graph
+    global result, graph
 
     number = graph[x][y][0]
-    graph[x][y][0] = -1 # 상어가 먹음
-    
+    graph[x][y][0] = -1  # 상어가 먹음
+
     fish_move(x, y)
     food = shark_food(x, y)
 
-    shark = max(shark, total + number)
+    result = max(result, total + number)
 
     for nx, ny in food:
         temp = deepcopy(graph)
         dfs(nx, ny, total + number)
         graph = deepcopy(temp)
-
-
 
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, -1, -1, -1, 0, 1, 1, 1]
@@ -62,11 +56,11 @@ dy = [0, -1, -1, -1, 0, 1, 1, 1]
 graph = [[0] * 4 for _ in range(4)]
 
 for i in range(4):
-    a, a1, b, b1, c, c1, d, d1 = map(int, input().split())
-    graph[i][0] = [a, a1 - 1]
-    graph[i][1] = [b, b1 - 1]
-    graph[i][2] = [c, c1 - 1]
-    graph[i][3] = [d, d1 - 1]
+    n1, d1, n2, d2, n3, d3, n4, d4 = map(int, input().split())
+    graph[i][0] = [n1, d1 - 1]
+    graph[i][1] = [n2, d2 - 1]
+    graph[i][2] = [n3, d3 - 1]
+    graph[i][3] = [n4, d4 - 1]
 
 dfs(0, 0, 0)
-print(shark)
+print(result)
--- 백준_문제풀이/20061.py
+++ 백준_문제풀이/20061.py
@@ -1,120 +1,105 @@
+def move(t, x, y):
+    if t == 1: # 1 * 1 크기
+        for d in range(2):
+            nx, ny = x, y
+            while 1: # 밑으로 보내기
+                if 0 <= nx + dx[d] < 10 and 0 <= ny + dy[d] < 10:
+                    if graph[nx + dx[d]][ny + dy[d]] == 0:
+                        nx = nx + dx[d]
+                        ny = ny + dy[d]
+                    else:
+                        break
+                else:
+                    break
+            graph[nx][ny] = 1
+    elif t == 2:  # 1 * 2 크기의 벽돌
+        for d in range(2):
+            lx, ly = x, y
+            rx, ry = x, y + 1
+            while 1:
+                if 0 <= lx + dx[d] < 10 and 0 <= ly + dy[d] < 10 and 0 <= rx + dx[d] < 10 and 0 <= ry + dy[d] < 10:
+                    if graph[lx + dx[d]][ly + dy[d]] == 0 and graph[rx + dx[d]][ry + dy[d]] == 0:
+                        lx, ly = lx + dx[d], ly + dy[d]
+                        rx, ry = rx + dx[d], ry + dy[d]
+                    else:
+                        break
+                else:
+                    break
+            graph[lx][ly] = 1
+            graph[rx][ry] = 1
+    elif t == 3:  # 2 * 1 크기의 벽돌
+        for d in range(2):
+            lx, ly = x, y
+            rx, ry = x + 1, y
+            while 1:
+                if 0 <= lx + dx[d] < 10 and 0 <= ly + dy[d] < 10 and 0 <= rx + dx[d] < 10 and 0 <= ry + dy[d] < 10:
+                    if graph[lx + dx[d]][ly + dy[d]] == 0 and graph[rx + dx[d]][ry + dy[d]] == 0:
+                        lx, ly = lx + dx[d], ly + dy[d]
+                        rx, ry = rx + dx[d], ry + dy[d]
+                    else:
+                        break
+                else:
+                    break
+            graph[lx][ly] = 1
+            graph[rx][ry] = 1
 
-n = int(input())
+def check(): # 4개가 가득 찼는지 확인
+    global graph, result
+    for i in range(6, 10):
+        if graph[i][0] == 1:
+            if graph[i][1] == graph[i][2] == graph[i][3] == 1: # 가득 찼으니 점수 올려주고, 빼주고, 5번에 새로운거를 넣어줌
+                result += 1
+                graph.pop(i)
+                graph.insert(4, [0] * 10)
+        else:
+            continue
+    for i in range(4, 6): # 연한 초록색 확인
+        for j in range(4):
+            if graph[i][j] == 1:
+                graph.pop()
+                graph.insert(4, [0] * 10)
+                break
 
-# t = 1이면 x, y
-# t = 2이면 x, y부터 오른쪽으로 붙어있는 타일
-# t = 3이면 x, y부터 아래로 붙어있는 타일
-tile = [list(map(int ,input().split())) for _ in range(n)]
+    board = [[0] * 10 for _ in range(10)]
+    for i in range(10):
+        for j in range(10):
+            board[j][i] = graph[i][j]
 
+    for i in range(6, 10):
+        if board[i][0] == 1:
+            if board[i][1] == board[i][2] == board[i][3] == 1: # 가득 찼으니 점수 올려주고, 빼주고, 5번에 새로운거를 넣어줌
+                result += 1
+                board.pop(i)
+                board.insert(4, [0] * 10)
+        else:
+            continue
+    for i in range(4, 6): # 연한 초록색 확인
+        for j in range(4):
+            if board[i][j] == 1:
+                board.pop()
+                board.insert(4, [0] * 10)
+                break
+    for i in range(10):
+        for j in range(10):
+            graph[j][i] = board[i][j]
+
+n = int(input()) # 블록을 놓을 횟수
 graph = [[0] * 10 for _ in range(10)]
+
+dx = [1, 0] # 밑으로, 오른쪽으로
+dy = [0, 1]
+
 result = 0
+for _ in range(n):
+    t, x, y = map(int, input().split()) # 블록의 형태 t, 좌표 (x, y)
+    move(t, x, y)
+    check()
 
-def move_blue(t, x, y):
-    if t == 1:
-        while 0 <= y < 10 and graph[x][y] == 0:
-            y += 1
-        graph[x][y - 1] = 1
-    elif t == 2:
-        while 0 <= y < 10 and graph[x][y] == 0:
-            y += 1
-        graph[x][y - 1] = 1
-        graph[x][y - 2] = 1
-    elif t == 3:
-        while 0 <= y < 10 and (graph[x][y] == 0 and graph[x + 1][y] == 0):
-            y += 1
-        graph[x][y - 1] = 1
-        graph[x + 1][y - 1] = 1
-
-def move_green(t, x, y):
-    if t == 1:
-        while 0 <= x < 10 and graph[x][y] == 0:
-            x += 1
-        graph[x - 1][y] = 1
-    elif t == 2:
-        while 0 <= x < 10 and (graph[x][y] == 0 and graph[x][y + 1] == 0):
-            x += 1
-        graph[x - 1][y] = 1
-        graph[x - 1][y + 1] = 1
-    elif t == 3:
-        while 0 <= x < 10 and (graph[x][y] == 0):
-            x += 1
-        graph[x - 1][y] = 1
-        graph[x - 2][y] = 1
-
-def thick_check():
-    global result
-    # 진한 파란색 검사
-    for i in range(6, 10):
-        tmp = 0
-        for j in range(4):
-            if graph[j][i] == 1:
-                tmp += 1
-        if tmp == 4:
-            s = [[0] * 10 for _ in range(10)]
-            for x in range(10):
-                for y in range(10):
-                    s[y][x] = graph[x][y]
-            
-            s.pop(i)
-            s.insert(4, [0] * 10)
-            result += 1
-            for x in range(10):
-                for y in range(10):
-                    graph[x][y] = s[y][x]
-    
-    # 진한 초록색 검사
-    for i in range(6, 10):
-        tmp = 0
-        for j in range(4):
-            if graph[i][j] == 1:
-                tmp += 1
-        if tmp == 4:
-            graph.pop(i)
-            graph.insert(4, [0] * 10)
-            result += 1
-
-def thin_check():
-    global result
-
-    for i in range(4, 6):
-        tmp = 0
-        for j in range(4):
-            if graph[i][j] == 1:
-                tmp += 1
-        if tmp >= 1:
-            graph.pop()
-            graph.insert(4, [0] * 10)
-    
-    for i in range(4, 6):
-        tmp = 0
-        for j in range(4):
-            if graph[j][i] == 1:
-                tmp += 1
-        if tmp >= 1:
-            s = [[0] * 10 for _ in range(10)]
-            for x in range(10):
-                for y in range(10):
-                    s[y][x] = graph[x][y]
-            
-            s.pop()
-            s.insert(4, [0] * 10)
-            for x in range(10):
-                for y in range(10):
-                    graph[x][y] = s[y][x]        
-
-
-for i in tile:
-    x, y, t = i
-    move_blue(x, y, t)
-    move_green(x, y, t)
-    thick_check()
-    thin_check()
-    
 answer = 0
 for i in range(10):
     for j in range(10):
         if graph[i][j] == 1:
-            answer += 1
+          answer += 1
 
 print(result)
 print(answer)
