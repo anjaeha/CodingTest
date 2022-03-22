56e3be6f4f06eb638b865b3a73d2f093541aff61
anjaeha <vix849@gmail.com>
2022-03-22 11:53:07 +0900

    2022.03.22

--- 백준_문제풀이/19237.py
+++ 백준_문제풀이/19237.py
@@ -1,104 +1,79 @@
-from copy import deepcopy
-
-# 현재 상어의 위치 찾기
-def shark_find(idx):
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == idx:
-                return (i, j)
-
-# 상어 움직이기
-def shark_move():
-    global graph
-    q = []
-    s = [[0] * n for _ in range(n)]
-    for idx in range(1, m + 1):
-        d = shark_d[idx] # 현재 상어의 방향
-        if d == 0: # 상어의 방향이 0이면 죽은 것이므로 진행할 필요가 없음.
-            continue
-        pos = shark_find(idx)
-        x, y = pos[0], pos[1] # 현재 상어의 위치
-        temp = priority_d[idx][d] # 현재 상어의 방향에서 우선순위 방향
-        flag = True
-        for i in range(1, 5):
-            nx = x + dx[temp[i]]
-            ny = y + dy[temp[i]]
-
-            if 0 <= nx < n and 0 <= ny < n:
-                if smell[nx][ny] == 0: # 냄새가 없는 칸으로 이동할 수 있게끔
-                    if s[nx][ny]: # 겹치면 숫자가 작은 상어만 냄새 뿌릴수 있게 해줌.
-                        if s[nx][ny] < idx: # 상어가 겹치면 작은 상어만 살아남음
-                            shark_d[idx] = 0 # 상어가 나가면 방향을 0으로 바꿔서 1만 살아있는지 확인할수 있도록 함
-                        else:
-                            shark_d[s[nx][ny]] = 0 # 상어의 방향만 0으로 설정해줌.
-                    s[nx][ny] = idx
-                    q.append((nx, ny, idx, temp[i])) # 겹치는곳이 있는지 확인하고 smell에 넣어줌
-                    flag = False
-                    break
-        
-        # 4방향 중 냄새가 없는 방향이 없으면
-        if flag:
-            for i in range(1, 5):
-                nx = x + dx[temp[i]]
-                ny = y + dy[temp[i]]
-
-                if 0 <= nx < n and 0 <= ny < n:
-                    if smell[nx][ny][0] == idx: # 자신의 냄새가 있는 방향으로 감
-                        shark_d[idx] = temp[i]
-                        smell[nx][ny][1] = k
-                        s[nx][ny] = idx
-                        break
-        
-    for i in range(len(q)): # 상어가 새로 이동한 장소를 저장한 값중, 상어가 살아있을때만 값을 넣어줌.
-        a, b, c, d = q[i]
-        if shark_d[c] != 0:
-            s[a][b] = c
-            smell[a][b] = [c, k]
-            shark_d[c] = d
-        
-    graph = deepcopy(s)
-
-def first_smell():
-    for idx in range(1, m + 1):
-        pos = shark_find(idx)
-        x, y = pos[0], pos[1] # 현재 상어의 위치
-        smell[x][y] = [idx, k]
-
-def spread_smell():
-    for i in range(n):
-        for j in range(n):
-            if smell[i][j] != 0: # smell 이 0이 아니면 1을 빼주고, 0이 되면 0으로 초기화해줌.
-                smell[i][j][1] -= 1
-                if smell[i][j][1] == -1:
-                    smell[i][j] = 0
-
-
-n, m, k = map(int, input().split())
-# n * n 격자에 m 마리의 상어, K초 후에 냄새 사라짐
+n, m, k = map(int, input().split()) # N * N크기의 격자, M마리의 상어, K초 후에 냄새 없어짐
 graph = [list(map(int, input().split())) for _ in range(n)]
+dir = [0] + list(map(int, input().split()))
+direction = [[[] for _ in range(5)]] + [[[0]] + [list(map(int, input().split())) for _ in range(4)] for _ in range(m)]
 
 dx = [0, -1, 1, 0, 0]
 dy = [0, 0, 0, -1, 1]
-# 위 아래 왼쪽 오른쪽
 
-shark_d = [0] + list(map(int, input().split())) # 현재 상어의 방향
+shark = {} # 상어 위치 찾기
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 0:
+            shark[graph[i][j]] = (i, j)
+            graph[i][j] = [graph[i][j], k]
 
-priority_d = [[]] # 상어 방향 우선순위
-s = [[]]
-for i in range(4 * m):    
-    temp = [0] + list(map(int, input().split()))
-    s.append((temp))
-    if i % 4 == 3:
-        priority_d.append(s)
-        s = [[]]
+def move(): # 상어 이동
+    global graph
+    board = [i[:] for i in graph]
+    for idx in range(1, m + 1):
+        temp = shark[idx]
+        if temp == (-1, -1):
+            continue
+        x, y = temp[0], temp[1]
+        for d in direction[idx][dir[idx]]:
+            nx = x + dx[d]
+            ny = y + dy[d]
 
-smell = [[0] * n for _ in range(n)] # 냄새를 기억하기 위한 그래프
-first_smell() # 첫번째 위치에 냄새를 뭍여줌
-result = 0
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == 0:
+                    if board[nx][ny] == 0:
+                        board[nx][ny] = [idx, k]
+                        dir[idx] = d
+                        shark[idx] = (nx, ny)
+                        break
+                    else:
+                        dir[idx] = -1
+                        shark[idx] = (-1, -1)
+                        break
+        else:
+            for d in direction[idx][dir[idx]]:
+                nx = x + dx[d]
+                ny = y + dy[d]
 
-while sum(shark_d[2:]) != 0 and result <= 1000:
-    spread_smell()
-    shark_move() # 상어 이동
-    result += 1
+                if 0 <= nx < n and 0 <= ny < n:
+                    if graph[nx][ny] != 0 and graph[nx][ny][0] == idx:
+                        board[nx][ny] = [idx, k]
+                        dir[idx] = d
+                        shark[idx] = (nx, ny)
+                        break
+    graph = [i[:] for i in board]
 
-print(result if result <= 1000 else -1)
+def spread(): # 냄새 빼주기, 0초남았을때도 효과는 남아있음.
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] != 0:
+                graph[i][j][1] -= 1
+
+                if graph[i][j][1] == -1:
+                    graph[i][j] = 0
+
+
+def check(): # 상어가 다 빠졌는지 확인하기
+    for i in range(2, m + 1):
+        if shark[i] != (-1, -1):
+            return False
+    return True
+
+
+cnt = 0
+while 1:
+    cnt += 1
+    spread()
+    move()
+    if check():
+        break
+    if cnt >= 1000:
+        cnt = -1
+        break
+print(cnt)
--- 백준_문제풀이/19238.py
+++ 백준_문제풀이/19238.py
@@ -6,92 +6,83 @@
 # 이동 도중 바닥나면 이동 실패, 업무 종료
 # 목적지까지 데려다 줄 수 있는지 확인하여 남은 연료의 양 출력
 from collections import deque
+def dist(x, y):
+    # 현재 위치에서 모든 지점까지의 거리 구하기
+    visit = [[-1] * n for _ in range(n)]
+    q = deque()
+    q.append((x, y))
+    visit[x][y] = 0
+    while q:
+        x, y = q.popleft()
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == 0 and visit[nx][ny] == -1:
+                    visit[nx][ny] = visit[x][y] + 1
+                    q.append((nx, ny))
+    # 어떤 승객을 태우러 갈지 결정
+    MIN = int(1e9)
+    idx = -1
+    for i in range(len(passenger)):
+        temp = passenger[i]
+        sx, sy = temp[0], temp[1]
+        if visit[sx][sy] < MIN:
+            MIN = visit[sx][sy]
+            idx = i
+
+    return idx, MIN
+
+# 필요한 연료 구하기
+def fuel(sx, sy, ex, ey):
+    visit = [[-1] * n for _ in range(n)]
+    visit[sx][sy] = 0
+    q = deque()
+    q.append((sx, sy))
+
+    while q:
+        x, y = q.popleft()
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == 0 and visit[nx][ny] == -1:
+                    visit[nx][ny] = visit[x][y] + 1
+                    q.append((nx, ny))
+    return visit[ex][ey]
+
+n, m, k = map(int, input().split())
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-n, m, k = map(int, input().split()) # 지도의 크기, 승객의 수, 최초 연료
-graph = [list(map(int, input().split())) for _ in range(n)]
-start_x, start_y = map(int, input().split()) # 택시의 시작 위치
-start_x, start_y = start_x - 1, start_y - 1
-p = deque()
-for i in range(m):
+graph = [list(map(int, input().split())) for _ in range(n)] # 0 - 빈칸, 1 - 벽
+tx, ty = map(int, input().split())
+tx, ty = tx - 1, ty - 1
+
+passenger = []
+for _ in range(m):
     sx, sy, ex, ey = map(int, input().split())
-    p.append((sx - 1, sy - 1, ex - 1, ey - 1))
+    passenger.append((sx - 1, sy - 1, ex - 1, ey - 1))
 
-def find_p(x, y): # 현재 위치에서 승객까지의 이동거리 찾기 (승객 선택하는 함수)
-    q = deque()
-    q.append((x, y))
-    s = [[-1] * n for _ in range(n)]
-    visit = [[False] * n for _ in range(n)]
-    s[x][y] = 0
-    visit[x][y] = True
-    while q:
-        x, y = q.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
+passenger.sort(key = lambda x : (x[0], x[1]))
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] == 0 and not visit[nx][ny]:
-                    s[nx][ny] = s[x][y] + 1
-                    visit[nx][ny] = True
-                    q.append((nx, ny))
-    dist = []
-    for i in range(len(p)):
-        cur = p[i]
-        if s[cur[0]][cur[1]] == -1:
-            continue
-        dist.append((s[cur[0]][cur[1]], cur[0], cur[1], cur[2], cur[3]))
-
-    dist.sort(key = lambda x : (x[0], x[1], x[2]))
-    if not dist: # 현재 위치에서 이동시킬 고객이 없으면 False값 리턴해줌.
-        return False
-    return dist[0]
-
-def calc_food(sx, sy, ex, ey): # 시작위치에서 도착위치까지의 거리 계산
-    q = deque()
-    q.append((sx, sy))
-    s = [[-1] * n for _ in range(n)]
-    visit = [[False] * n for _ in range(n)]
-    visit[sx][sy] = True
-    s[sx][sy] = 0
-    while q:
-        x, y = q.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-
-            if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] == 0 and not visit[nx][ny]:
-                    visit[nx][ny] = True
-                    s[nx][ny] = s[x][y] + 1
-                    q.append((nx, ny))
-    return s[ex][ey]
-
-flag = True
-for case in range(m):
-    cur = find_p(start_x, start_y) # 현재 위치에서 최단 경로에 있는 승객 구함
-    if not cur: # 현재 위치에서 이동할 수 있는 승객이 없으면 종료
-        flag = False
+for i in range(m):
+    idx, idx_cost = dist(tx, ty)
+    # 갈수 없는 곳이라면 종료
+    if idx_cost == -1:
+        k = -1
         break
-    food = calc_food(cur[1], cur[2], cur[3], cur[4])
-    if food == -1: # 이동할 수 없는 곳에 있으면 종료
-        flag = False
+    k -= idx_cost # 찾아가는데 연료 사용
+
+    need_fuel = fuel(passenger[idx][0], passenger[idx][1], passenger[idx][2], passenger[idx][3]) # 손님을 태우고 이동하는데 필요한 연료
+    if k < need_fuel:
+        k = -1
         break
+    k += need_fuel
+    tx, ty = passenger[idx][2], passenger[idx][3]
+    passenger.pop(idx)
 
-    k -= cur[0]
-    if k < food: # 현재 가지고 있는 연료보다 필요한 연료가 더 크면 운행 종료
-        flag = False
-        break
-    else:
-        k += food
-        start_x, start_y = cur[3], cur[4]
-        cc = p.index((cur[1], cur[2], cur[3], cur[4]))
-        del p[cc]
-
-
-
-if flag:
-    print(k)
-else:
-    print(-1)
+print(k)
