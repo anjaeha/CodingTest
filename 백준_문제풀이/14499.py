88502ea8b5e379ba21e3a4a0ad2efd06651529fd
anjaeha <vix849@gmail.com>
2022-04-04 11:40:47 +0900

    2022.04.04

--- 백준_문제풀이/14499.py
+++ 백준_문제풀이/14499.py
@@ -1,37 +1,35 @@
-# 주사위의 모든 면에는 0이 적혀있다.
-# 이동 칸의 수가 0이면, 주사위의 바닥면이 복사
-# 0이 아니면, 주사위의 바닥에 복사하고 바닥을 0으로 바꿈
+n, m, x, y, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+dir = list(map(int, input().split()))
 
-# 동, 서, 북, 남
+dice = [0, 0, 0, 0, 0, 0, 0]
 dx = [0, 0, 0, -1, 1]
 dy = [0, 1, -1, 0, 0]
 
-n, m, x, y, k = map(int, input().split()) # 세로N, 가로M, 주사위좌표 X, Y, 명령의 개수 K
-graph = [list(map(int, input().split())) for _ in range(n)]
-order = list(map(int, input().split()))
+def move(d):
+    global dice, graph, x, y
+    nx = x + dx[d]
+    ny = y + dy[d]
 
-dice = [0, 0, 0, 0, 0, 0, 0] # 1이 윗면, 6이 바닥
+    if nx < 0 or ny < 0 or nx >= n or ny >= m: # 범위를 벗어나면 무시
+        return
 
-def move(d): # 주사위 굴리기
-    global dice
-    if d == 1: # 동쪽으로 굴리기
+    if d == 1: # 동쪽으로
         dice = [0, dice[4], dice[2], dice[1], dice[6], dice[5], dice[3]]
-    elif d == 2: # 서쪽으로 굴리기
+    elif d == 2: # 서쪽으로
         dice = [0, dice[3], dice[2], dice[6], dice[1], dice[5], dice[4]]
-    elif d == 3: # 북쪽으로 굴리기
+    elif d == 3: # 북쪽으로
         dice = [0, dice[5], dice[1], dice[3], dice[4], dice[6], dice[2]]
-    elif d == 4: # 남쪽으로 굴리기
+    elif d == 4: # 남쪽으로
         dice = [0, dice[2], dice[6], dice[3], dice[4], dice[1], dice[5]]
 
-for i in range(k):
-    d = order[i]
-    nx, ny = x + dx[d], y + dy[d]
-    if 0 <= nx < n and 0 <= ny < m:
-        move(d)
-        if graph[nx][ny] == 0:
-            graph[nx][ny] = dice[6]
-        else:
-            dice[6] = graph[nx][ny]
-            graph[nx][ny] = 0
-        x, y = nx, ny
-        print(dice[1])
+    if graph[nx][ny] == 0: # 이동한 칸이 0이면
+        graph[nx][ny] = dice[6] # 주사위 밑면을 복사
+    else:
+        dice[6] = graph[nx][ny] # 밑면에 값을 넣고
+        graph[nx][ny] = 0 # 0으로 변경
+    print(dice[1]) # 주사위의 윗면을 출력
+    x, y = nx, ny
+
+for d in dir:
+    move(d)
--- 백준_문제풀이/14500.py
+++ 백준_문제풀이/14500.py
@@ -1,44 +1,42 @@
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def find(idx, x, y, SUM):
+def dfs(x, y, depth, result):
     global MAX
-    if idx == 4:
-        MAX = max(MAX, SUM)
+    if depth == 4:
+        MAX = max(MAX, result)
         return
 
-    if SUM + (4 - idx) * mNumber <= MAX:
+    if result + (4 - depth) * mNumber <= MAX:
         return
 
     for d in range(4):
         nx = x + dx[d]
         ny = y + dy[d]
-        if 0 <= nx < n and 0 <= ny < m:
-            if not visit[nx][ny]:
+
+        if 0 <= nx < n and 0 <= ny < m and not visit[nx][ny]:
+            visit[nx][ny] = True
+            dfs(nx, ny, depth + 1, result + graph[nx][ny])
+            visit[nx][ny] = False
+
+            if depth == 2:
                 visit[nx][ny] = True
-                find(idx + 1, nx, ny, SUM + graph[nx][ny])
+                dfs(x, y, depth + 1, result + graph[nx][ny])
                 visit[nx][ny] = False
-
-                if idx == 2:
-                    visit[nx][ny] = True
-                    find(idx + 1, x, y, SUM + graph[nx][ny])
-                    visit[nx][ny] = False
-
 
 mNumber = -1
 for i in range(n):
-    for j in range(m):
-        mNumber = max(mNumber, graph[i][j])
+    mNumber = max(mNumber, max(graph[i]))
 
-
-visit = [[False] * m for _ in range(n)]
 MAX = -1
-for i in range(n):
-    for j in range(m):
-        visit[i][j] = True
-        find(1, i, j, graph[i][j])
-        visit[i][j] = False
+visit = [[False] * m for _ in range(n)]
+for x in range(n):
+    for y in range(m):
+        visit[x][y] = True
+        dfs(x, y, 1, graph[x][y])
+        visit[x][y] = False
 
 print(MAX)
