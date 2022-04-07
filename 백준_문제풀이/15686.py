cfc23b3d9f038fe718a6c77c3ff3d5fd9b4876d9
anjaeha <vix849@gmail.com>
2022-04-07 11:46:00 +0900

    2022.04.07

--- 백준_문제풀이/15686.py
+++ 백준_문제풀이/15686.py
@@ -1,8 +1,9 @@
-n, m = map(int, input().split()) # 치킨집의 최대 개수 M
-graph = [list(map(int, input().split())) for _ in range(n)] # 빈 칸 - 0, 집 - 1, 치킨집 - 2
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-house_pos = []
+
 chicken_pos = []
+house_pos = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
@@ -10,36 +11,36 @@ for i in range(n):
         elif graph[i][j] == 2:
             chicken_pos.append((i, j))
 
-candi = []
-temp = []
 visit = [False] * len(chicken_pos)
-
-def dfs(depth):
+temp = []
+candi = []
+def make_candi(depth):
     if depth == m:
         candi.append(list(temp))
         return
+
     for i in range(len(chicken_pos)):
         if visit[i]:
             continue
         visit[i] = True
         temp.append(chicken_pos[i])
-        dfs(depth + 1)
+        make_candi(depth + 1)
         temp.pop()
+
         for j in range(i + 1, len(chicken_pos)):
             visit[j] = False
+make_candi(0)
 
-dfs(0)
+result = int(1e9)
+for idx in range(len(candi)):
+    now = candi[idx]
+    SUM = 0
+    for hx, hy in house_pos:
+        MIN = int(1e9)
+        for cx, cy in now:
+           temp = abs(hx - cx) + abs(hy - cy)
+           MIN = min(MIN, temp)
+        SUM += MIN
+    result = min(result, SUM)
 
-MIN = int(1e9)
-for case in range(len(candi)):
-    dist = 0
-    for i in range(len(house_pos)):
-        hx, hy = house_pos[i]
-        temp = int(1e9)
-        for j in range(m):
-            cx, cy = candi[case][j]
-            temp = min(abs(hx - cx) + abs(hy - cy), temp)
-        dist += temp
-
-    MIN = min(MIN, dist)
-print(MIN)
+print(result)
--- 백준_문제풀이/16236.py
+++ 백준_문제풀이/16236.py
@@ -11,57 +11,46 @@ for i in range(n):
         if graph[i][j] == 9:
             graph[i][j] = 0
             sx, sy = i, j
+            break
 size = 2
-
-result = 0
-def search():
-    global result
+def find_food():
+    dist = [[-1] * n for _ in range(n)]
+    dist[sx][sy] = 0
     q = deque()
     q.append((sx, sy))
-    visit = [[-1] * n for _ in range(n)] # 현재 위치에서 거리 구하기
-    can_eat = [[False] * n for _ in range(n)] # 먹을 수 있는지 확인
-    visit[sx][sy] = 0
+    food = []
+
     while q:
         x, y = q.popleft()
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
+
             if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] <= size and visit[nx][ny] == -1:
-                    visit[nx][ny] = visit[x][y] + 1
+                if graph[nx][ny] <= size and dist[nx][ny] == -1:
+                    dist[nx][ny] = dist[x][y] + 1
                     q.append((nx, ny))
                     if 0 < graph[nx][ny] < size:
-                        can_eat[nx][ny] = True
+                        food.append((dist[nx][ny], nx, ny))
 
-    MIN = int(1e9)
-    mx, my = -1, -1
-    for i in range(n):
-        for j in range(n):
-            if can_eat[i][j]: # 먹을 수 있는 곳이면
-                if visit[i][j] < MIN: # 최소거리이면서 가장 왼쪽, 가장 위에 있는 곳을 구해야함
-                    MIN = visit[i][j]
-                    mx, my = i, j
-
-    if (mx, my) == (-1, -1): # 만약, 최소거리가 없으면 종료
+    food.sort(key = lambda x : (x[0], x[1], x[2]))
+    if food:
+        return food[0]
+    else:
         return False
 
-    return (visit[mx][my], mx, my)
-
-
-size_cnt = 0
+result = 0
+eat = 0
 while 1:
-    res = search()
-    if res == False:
+    temp = find_food()
+    if not temp:
         break
-
-    count, mx, my = res
-
-    result += count
-    sx, sy = mx, my # 상어의 위치를 변경해주고
-    graph[sx][sy] = 0 # 먹었으니 0으로 변경
-    size_cnt += 1 # 몇개 먹었는지 확인
-    if size_cnt == size: # 먹은 개수가 몸의 크기와 같으면
-        size += 1 # 사이즈 키워주고 0으로 초기화
-        size_cnt = 0
+    result += temp[0]
+    sx, sy = temp[1], temp[2]
+    graph[sx][sy] = 0
+    eat += 1
+    if eat == size:
+        size += 1
+        eat = 0
 
 print(result)
