4cb442b213a0641d370299ff99d46eabda345ba8
anjaeha <vix849@gmail.com>
2022-04-13 11:52:05 +0900

    2022.04.13

--- 백준_문제풀이/19237.py
+++ 백준_문제풀이/19237.py
@@ -1,79 +1,77 @@
-n, m, k = map(int, input().split()) # N * N크기의 격자, M마리의 상어, K초 후에 냄새 없어짐
+n, m, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-dir = [0] + list(map(int, input().split()))
-direction = [[[] for _ in range(5)]] + [[[0]] + [list(map(int, input().split())) for _ in range(4)] for _ in range(m)]
+shark_dir = [0] + list(map(int, input().split()))
+dir = [[]] + [[[0]] + [list(map(int, input().split())) for _ in range(4)] for _ in range(m)]
 
 dx = [0, -1, 1, 0, 0]
 dy = [0, 0, 0, -1, 1]
 
-shark = {} # 상어 위치 찾기
+smell = [[0] * n for _ in range(n)]
+pos = {}
 for i in range(n):
     for j in range(n):
         if graph[i][j] != 0:
-            shark[graph[i][j]] = (i, j)
-            graph[i][j] = [graph[i][j], k]
+            pos[graph[i][j]] = (i, j)
+            graph[i][j] = 0
 
-def move(): # 상어 이동
-    global graph
-    board = [i[:] for i in graph]
+def spread(): # 냄새 뿌리기
     for idx in range(1, m + 1):
-        temp = shark[idx]
-        if temp == (-1, -1):
-            continue
-        x, y = temp[0], temp[1]
-        for d in direction[idx][dir[idx]]:
-            nx = x + dx[d]
-            ny = y + dy[d]
+        if idx in pos:
+            x, y = pos[idx]
+            smell[x][y] = [idx, k]
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if graph[nx][ny] == 0:
-                    if board[nx][ny] == 0:
-                        board[nx][ny] = [idx, k]
-                        dir[idx] = d
-                        shark[idx] = (nx, ny)
-                        break
-                    else:
-                        dir[idx] = -1
-                        shark[idx] = (-1, -1)
-                        break
-        else:
-            for d in direction[idx][dir[idx]]:
+def shark_move(): # 상어 움직이기
+    for idx in range(1, m + 1):
+        if idx in pos:
+            x, y = pos[idx]
+            temp = shark_dir[idx] # 현재 방향에서 우선순위
+
+            for d in dir[idx][temp]:
                 nx = x + dx[d]
                 ny = y + dy[d]
 
                 if 0 <= nx < n and 0 <= ny < n:
-                    if graph[nx][ny] != 0 and graph[nx][ny][0] == idx:
-                        board[nx][ny] = [idx, k]
-                        dir[idx] = d
-                        shark[idx] = (nx, ny)
+                    if smell[nx][ny] == 0:
+                        pos[idx] = (nx, ny)
+                        shark_dir[idx] = d
                         break
-    graph = [i[:] for i in board]
+            else:
+                for d in dir[idx][temp]:
+                    nx = x + dx[d]
+                    ny = y + dy[d]
+                    if 0 <= nx < n and 0 <= ny < n:
+                        if smell[nx][ny][0] == idx:
+                            pos[idx] = (nx, ny)
+                            shark_dir[idx] = d
+                            break
+    for idx in range(1, m + 1): # 겹칠경우 제외해주기
+        if idx in pos:
+            for j in range(idx + 1, m + 1):
+                if j in pos:
+                    if pos[idx] == pos[j]:
+                        del pos[j]
 
-def spread(): # 냄새 빼주기, 0초남았을때도 효과는 남아있음.
+def smell_down(): # 냄새 1 감소
     for i in range(n):
         for j in range(n):
-            if graph[i][j] != 0:
-                graph[i][j][1] -= 1
+            if smell[i][j]:
+                smell[i][j][1] -= 1
+                if smell[i][j][1] == 0:
+                    smell[i][j] = 0
 
-                if graph[i][j][1] == -1:
-                    graph[i][j] = 0
-
-
-def check(): # 상어가 다 빠졌는지 확인하기
-    for i in range(2, m + 1):
-        if shark[i] != (-1, -1):
+def check(): # 1을 제외한 다른 상어가 남아있는지 확인
+    for idx in range(2, m + 1):
+        if idx in pos:
             return False
     return True
 
-
-cnt = 0
+result = 0
 while 1:
-    cnt += 1
+    if check() or result > 1000:
+        break
+    result += 1
     spread()
-    move()
-    if check():
-        break
-    if cnt >= 1000:
-        cnt = -1
-        break
-print(cnt)
+    shark_move()
+    smell_down()
+
+print(result if result != 1001 else -1)
