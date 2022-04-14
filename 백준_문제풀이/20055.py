f160422caadb3040369227ad39e9f1fe8f0f019f
anjaeha <vix849@gmail.com>
2022-04-14 11:46:08 +0900

    2022.04.14

--- 백준_문제풀이/20055.py
+++ 백준_문제풀이/20055.py
@@ -1,53 +1,44 @@
-# 1번에서 올리고, N번에서 내림
-# 로봇을 올리거나 움직이면 내구도 -1
-# 벨트 회전 -> 로봇 이동 -> 로봇 올림 -> 내구도 검사
+n, k = map(int, input().split())
+belt = list(map(int, input().split()))
 
-def belt_move():
-    down_belt.insert(0, up_belt.pop())
-    up_belt.insert(0, down_belt.pop())
-    robot.pop()
-    robot.insert(0, False)
+robot = [False] * (2 * n)
+
+def rotate():
+    belt.insert(0, belt.pop())
+    robot.insert(0, robot.pop())
 
 def robot_move():
-    for idx in range(n - 1, -1, -1):
+    for idx in range(n, -1, -1):
         if robot[idx]:
-            if idx == n - 1:
+            if idx == n - 1 or idx == n:
                 robot[idx] = False
             else:
-                if not robot[idx + 1] and up_belt[idx + 1] > 0:
+                if belt[idx + 1] > 0 and not robot[idx + 1]:
+                    belt[idx + 1] -= 1
                     robot[idx + 1] = True
                     robot[idx] = False
-                    up_belt[idx + 1] -= 1
 
-def raise_robot():
-    if up_belt[0] > 0:
+def robot_raise():
+    if belt[0] > 0:
         robot[0] = True
-        up_belt[0] -= 1
+        belt[0] -= 1
 
 def check():
     cnt = 0
-    for i in range(n):
-        if up_belt[i] == 0:
-            cnt += 1
-        if down_belt[i] == 0:
+    for i in range(2 * n):
+        if belt[i] == 0:
             cnt += 1
     if cnt >= k:
         return True
     return False
 
-n, k = map(int, input().split())
-belt = list(map(int, input().split()))
-up_belt = belt[:n]
-down_belt = belt[n:]
-robot = [False] * n
-
-count = 1
+result = 1
 while 1:
-    belt_move()
+    rotate()
     robot_move()
-    raise_robot()
+    robot_raise()
     if check():
         break
-    count += 1
+    result += 1
 
-print(count)
+print(result)
--- 백준_문제풀이/20056.py
+++ 백준_문제풀이/20056.py
@@ -1,69 +1,60 @@
+n, m, k = map(int, input().split())
+graph = [[0] * n for _ in range(n)]
+for _ in range(m):
+    x, y, w, s, d = map(int, input().split())
+    graph[x - 1][y - 1] = [[w, s, d]] # 질량 속력 방향
+
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
-n, m, k = map(int, input().split())
-graph = [[0] * n for _ in range(n)]
-
-for i in range(m):
-    x, y, w, s, d = map(int, input().split()) # (x, y)에 질량w, 속도 s, 방향d
-    graph[x - 1][y - 1] = [[w, s, d]]
-
-def ball_move():
+def move(): # 파이어볼 이동
     global graph
-    board = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == 0:
-                continue
-            for c in range(len(graph[i][j])):
-                w, s, d = graph[i][j][c] # 질량 속도 방향
-                nx = (i + dx[d] * s) % n
-                ny = (j + dy[d] * s) % n
-                if board[nx][ny] == 0:
-                    board[nx][ny] = [[w, s, d]]
+    temp_graph = [[0] * n for _ in range(n)]
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y]:
+                for idx in range(len(graph[x][y])):
+                    now = graph[x][y][idx]
+                    nx = (x + dx[now[2]] * now[1]) % n
+                    ny = (y + dy[now[2]] * now[1]) % n
+
+                    if temp_graph[nx][ny] == 0:
+                        temp_graph[nx][ny] = [now]
+                    else:
+                        temp_graph[nx][ny].append(now)
+    graph = temp_graph
+
+def div_fireball():
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] and len(graph[x][y]) > 1:
+                weight, speed, cnt, dire = 0, 0, 0, 0 # 합쳐진 질량, 속력, 개수, 방향
+                for idx in range(len(graph[x][y])):
+                    now = graph[x][y][idx]
+                    weight += now[0]
+                    speed += now[1]
+                    dire += now[2] % 2 # 홀수인지 짝수인지만 알면됨
+                    cnt += 1
+                if weight >= 5:
+                    graph[x][y] = []
+                    if dire == cnt or dire == 0:
+                        for i in range(4):
+                           graph[x][y].append([weight // 5, speed // cnt, i * 2])
+                    else:
+                        for i in range(4):
+                            graph[x][y].append([weight // 5, speed // cnt, i * 2 + 1])
                 else:
-                    board[nx][ny].append((w, s, d))
+                    graph[x][y] = 0
 
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 0:
-                continue
-            if len(board[i][j]) <= 1:
-                continue
-            sum_w = 0 # 질량의 합
-            sum_s = 0 # 속력의 합
-            sum_i = 0 # 합쳐진 개수
-            sum_d = 0 # 방향
-            for c in range(len(board[i][j])):
-                w, s, d = board[i][j][c]  # 질량 속도 방향
-                sum_w += w
-                sum_s += s
-                sum_i += 1
-                sum_d += d % 2
+for i in range(k):
+    move()
+    div_fireball()
 
-            board[i][j] = []
-            cnt_w = sum_w // 5 # 나누어진 파이어볼의 질량
-            cnt_s = sum_s // sum_i # 나누어진 파이어볼의 속력
-            if cnt_w == 0:
-                continue
-            if sum_d == sum_i or sum_d == 0: # 모두 짝수이거나 홀수이면
-                for idx in range(4):
-                    board[i][j].append((cnt_w, cnt_s, idx * 2))
-            else:
-                for idx in range(4):
-                    board[i][j].append((cnt_w, cnt_s, idx * 2 + 1))
-    graph = board
-
-
-for _ in range(k):
-    ball_move()
 
 result = 0
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 0:
-            continue
-        for c in range(len(graph[i][j])):
-            result += graph[i][j][c][0]
-
+for x in range(n):
+    for y in range(n):
+        if graph[x][y]:
+            for i in range(len(graph[x][y])):
+                result += graph[x][y][i][0]
 print(result)
