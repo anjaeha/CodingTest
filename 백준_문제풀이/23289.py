97c857002c7a83f6336d34a8fa09905f1f31992a
anjaeha <vix849@gmail.com>
2022-04-19 11:52:50 +0900

    2022.04.19

--- 백준_문제풀이/23289.py
+++ 백준_문제풀이/23289.py
@@ -1,10 +1,96 @@
-def check():
-    for x, y in search_pos:
-        if board[x][y] < k:
-            return False
-    return True
+n, m, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+w = int(input())
+wall = [[] for _ in range(4)]
+board = [[0] * m for _ in range(n)] # 온도 표시
 
-def border(): # 테두리 온도 감소
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+for _ in range(w):
+    x, y, d = map(int, input().split())
+    if d == 0: # 벽이 위에 있는 경우
+        wall[0].append((x - 1, y - 1)) # 위쪽에 벽이 있음
+        wall[1].append((x - 2, y - 1)) # 아래쪽에 벽이 있음
+    else: # 오른쪽에 벽이 있는 경우
+        wall[2].append((x - 1, y - 1)) # 오른쪽에 벽이 있음
+        wall[3].append((x - 1, y)) # 왼쪽에 벽이 있음
+
+check_pos = []
+machine_pos = [[] for _ in range(5)]
+for x in range(n):
+    for y in range(m):
+        if graph[x][y] == 5:
+            check_pos.append((x, y))
+        elif 0 < graph[x][y] < 5:
+            machine_pos[graph[x][y]].append((x, y))
+
+def machine_work(x, y, d):
+    if d == 1: # 방향이 오른쪽인 온풍기
+        now = [[] for _ in range(6)]
+        now[5].append((x, y + 1))
+        for idx in range(5, 1, -1):
+            for r, c in now[idx]:
+                if (r, c) not in wall[0] and (r - 1, c) not in wall[2] and 0 <= r - 1 < n and 0 <= c + 1 < m and (r - 1, c + 1) not in now[idx - 1]:
+                    now[idx - 1].append((r - 1, c + 1))
+                if (r, c) not in wall[2] and 0 <= r < n and 0 <= c + 1 < m and (r, c + 1) not in now[idx - 1]:
+                    now[idx - 1].append((r, c + 1))
+                if (r, c) not in wall[1] and (r + 1, c) not in wall[2] and 0 <= r + 1 < n and 0 <= c + 1 < m and (r + 1, c + 1) not in now[idx - 1]:
+                    now[idx - 1].append((r + 1, c + 1))
+    elif d == 2: # 방향이 왼쪽인 온풍기
+        now = [[] for _ in range(6)]
+        now[5].append((x, y - 1))
+        for idx in range(5, 1, -1):
+            for r, c in now[idx]:
+                if (r, c) not in wall[0] and (r - 1, c) not in wall[3] and 0 <= r - 1 < n and 0 <= c - 1 < m and (r - 1, c - 1) not in now[idx - 1]:
+                    now[idx - 1].append((r - 1, c - 1))
+                if (r, c) not in wall[3] and 0 <= r < n and 0 <= c - 1 < m and (r, c - 1) not in now[idx - 1]:
+                    now[idx - 1].append((r, c - 1))
+                if (r, c) not in wall[1] and (r + 1, c) not in wall[3] and 0 <= r + 1 < n and 0 <= c - 1 < m and (r + 1, c - 1) not in now[idx - 1]:
+                    now[idx - 1].append((r + 1, c - 1))
+    elif d == 3: # 방향이 위인 온풍기
+        now = [[] for _ in range(6)]
+        now[5].append((x - 1, y))
+        for idx in range(5, 1, -1):
+            for r, c in now[idx]:
+                if (r, c) not in wall[3] and (r, c - 1) not in wall[0] and 0 <= r - 1 < n and 0 <= c - 1 < m and (r - 1, c - 1) not in now[idx - 1]:
+                    now[idx - 1].append((r - 1, c - 1))
+                if (r, c) not in wall[0] and 0 <= r - 1 < n and 0 <= c < m and (r - 1, c) not in now[idx - 1]:
+                    now[idx - 1].append((r - 1, c))
+                if (r, c) not in wall[2] and (r, c + 1) not in wall[0] and 0 <= r - 1 < n and 0 <= c + 1 < m and (r - 1, c + 1) not in now[idx - 1]:
+                    now[idx - 1].append((r - 1, c + 1))
+    elif d == 4: # 방향이 아래인 온풍기
+        now = [[] for _ in range(6)]
+        now[5].append((x + 1, y))
+        for idx in range(5, 1, -1):
+            for r, c in now[idx]:
+                if (r, c) not in wall[3] and (r, c - 1) not in wall[1] and 0 <= r + 1 < n and 0 <= c - 1 < m and (r + 1, c - 1) not in now[idx - 1]:
+                    now[idx - 1].append((r + 1, c - 1))
+                if (r, c) not in wall[1] and 0 <= r + 1 < n and 0 <= c < m and (r + 1, c) not in now[idx - 1]:
+                    now[idx - 1].append((r + 1, c))
+                if (r, c) not in wall[2] and (r, c + 1) not in wall[1] and 0 <= r + 1 < n and 0 <= c + 1 < m and (r + 1, c + 1) not in now[idx - 1]:
+                    now[idx - 1].append((r + 1, c + 1))
+
+    for idx in range(1, 6):
+        for r, c in now[idx]:
+            board[r][c] += idx
+
+def tmp_control():
+    global board
+    temp_board = [i[:] for i in board]
+    for x in range(n):
+        for y in range(m):
+            if board[x][y] >= 4:
+                for d in range(4):
+                    if (x, y) not in wall[d]:
+                        nx = x + dx[d]
+                        ny = y + dy[d]
+                        if 0 <= nx < n and 0 <= ny < m and board[x][y] > board[nx][ny]:
+                            temp_board[x][y] -= (board[x][y] - board[nx][ny]) // 4
+                            temp_board[nx][ny] += (board[x][y] - board[nx][ny]) // 4
+    board = temp_board
+
+def border():
     for i in range(n):
         if board[i][0] > 0:
             board[i][0] -= 1
@@ -16,130 +102,25 @@ def border(): # 테두리 온도 감소
         if board[n - 1][i] > 0:
             board[n - 1][i] -= 1
 
-def machine_running(): # 온풍기 가동
-    for x, y, dir in machine:
-        arr = [[] for _ in range(6)]
-        if dir == 1: # 오른쪽으로 온풍기 발사
-            arr[5].append((x, y + 1))
-            for idx in range(5, 0, -1):
-                for r, c in arr[idx]:
-                    if (r - 1, c) not in wall[2] and (r - 1, c) not in wall[0]: # 아래나 오른쪽에 벽 있으면 못감
-                        if 0 <= (r - 1) < n and 0 <= (c + 1) < m and (r - 1, c + 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r - 1, c + 1))
-                    if (r, c) not in wall[0]: # 오른쪽에 벽 있으면 못감
-                        if 0 <= r < n and 0 <= c + 1 < m and (r, c + 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r, c + 1))
-                    if (r + 1, c) not in wall[3] and (r + 1, c) not in wall[0]: # 위에나 오른쪽에 벽 있으면 못감
-                        if 0 <= r + 1 < n and 0 <= c + 1 < m and (r + 1, c + 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r + 1, c + 1))
-        elif dir == 2: # 왼쪽으로 온풍기 발사
-            arr[5].append((x, y - 1))
-            for idx in range(5, 0, -1):
-                for r, c in arr[idx]:
-                    if (r - 1, c) not in wall[1] and (r - 1, c) not in wall[2]:
-                        if 0 <= r - 1 < n and 0 <= c - 1 < m and (r - 1, c - 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r - 1, c - 1))
-                    if (r, c) not in wall[1]:
-                        if 0 <= r < n and 0 <= c - 1 < m and (r, c - 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r, c - 1))
-                    if (r + 1, c) not in wall[1] and (r + 1, c) not in wall[3]:
-                        if 0 <= r + 1 < n and 0 <= c - 1 < m and (r + 1, c - 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r + 1, c - 1))
-        elif dir == 3: # 위쪽으로 온풍기 발사
-            arr[5].append((x - 1, y))
-            for idx in range(5, 0, -1):
-                for r, c in arr[idx]:
-                    if (r, c - 1) not in wall[3] and (r, c - 1) not in wall[0]:
-                        if 0 <= r - 1 < n and 0 <= c - 1 < m and (r - 1, c - 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r - 1, c - 1))
-                    if (r, c) not in wall[3]:
-                        if 0 <= r - 1 < n and 0 <= c < m and (r - 1, c) not in arr[idx - 1]:
-                            arr[idx - 1].append((r - 1, c))
-                    if (r, c + 1) not in wall[1] and (r, c + 1) not in wall[3]:
-                        if 0 <= r - 1 < n and 0 <= c + 1 < m and (r - 1, c + 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r - 1, c + 1))
-        elif dir == 4: # 아래쪽으로 온풍기 발사
-            arr[5].append((x + 1, y))
-            for idx in range(5, 0, -1):
-                for r, c in arr[idx]:
-                    if (r, c - 1) not in wall[0] and (r, c - 1) not in wall[2]:
-                        if 0 <= r + 1 < n and 0 <= c - 1 < m and (r + 1, c - 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r + 1, c - 1))
-                    if (r, c) not in wall[2]:
-                        if 0 <= r + 1 < n and 0 <= c < m and (r + 1, c) not in arr[idx - 1]:
-                            arr[idx - 1].append((r + 1, c))
-                    if (r, c + 1) not in wall[1] and (r, c + 1) not in wall[2]:
-                        if 0 <= r + 1 < n and 0 <= c + 1 < m and (r + 1, c + 1) not in arr[idx - 1]:
-                            arr[idx - 1].append((r + 1, c + 1))
-        for idx in range(1, 6):
-            for x, y in arr[idx]:
-                board[x][y] += idx
+def check():
+    for x, y in check_pos:
+        if board[x][y] < k:
+            return False
+    return True
 
-n, m, k = map(int, input().split()) # 그래프 (R, C), 조사하는 칸의 온도가 K이상이면 종료
-graph = [list(map(int, input().split())) for _ in range(n)] # 빈칸0, (우, 좌, 상, 하), 조사해야하는 칸5
-board = [[0] * m for _ in range(n)] # 온도 확인 그래프
+result = 0
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-search_pos = []
-machine = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 5:
-            search_pos.append((i, j))
-        elif 1 <= graph[i][j] <= 4:
-            machine.append((i, j, graph[i][j]))
-
-w = int(input())
-wall = [[] for _ in range(4)] # 벽의 위치를 동, 서, 남, 북으로 재설정
-for _ in range(w):
-    x, y, t = map(int, input().split()) # 벽의 위치, 0이면 위에있고, 1이면 오른쪽에 있다.
-    x, y = x - 1, y - 1
-    if t == 1:
-        wall[0].append((x, y)) # 오른쪽에 벽 있음
-        wall[1].append((x, y + 1)) # 왼쪽에 벽 있음
-    else:
-        wall[3].append((x, y)) # 위에 벽이 있음
-        wall[2].append((x - 1, y)) # 밑에 벽이 있음
-
-def spread(): # 온도 퍼지기
-    global board
-    new_board = [i[:] for i in board]
-    for x in range(n):
-        for y in range(m):
-            if board[x][y] >= 4:
-                for d in range(4): # 위 아래 왼쪽 오른쪽
-                    nx = x + dx[d]
-                    ny = y + dy[d]
-
-                    if 0 <= nx < n and 0 <= ny < m:
-                        if d == 0:
-                            if (x, y) in wall[3]:
-                                continue
-                        elif d == 1:
-                            if (x, y) in wall[2]:
-                                continue
-                        elif d == 2:
-                            if (x, y) in wall[1]:
-                                continue
-                        elif d == 3:
-                            if (x, y) in wall[0]:
-                                continue
-                        if board[x][y] > board[nx][ny]:
-                            new_board[x][y] -= (board[x][y] - board[nx][ny]) // 4
-                            new_board[nx][ny] += (board[x][y] - board[nx][ny]) // 4
-    board = new_board
-
-cnt = 0
 while 1:
-    machine_running()
-    spread()
+    for idx in range(1, 5):
+        temp = machine_pos[idx]
+        for i in range(len(temp)):
+            x = temp[i][0]
+            y = temp[i][1]
+            machine_work(x, y, idx)
+    tmp_control()
     border()
-    cnt += 1
-    if check():
-        break
-    if cnt > 100:
+    result += 1
+    if check() or result >= 101:
         break
 
-print(cnt)
+print(result)
--- 백준_문제풀이/23290.py
+++ 백준_문제풀이/23290.py
@@ -1,60 +1,54 @@
-# 4 * 4 그래프에서 움직임
-# M마리의 물고기와 상어, 8방향중 하나로 움직임, 한 칸에 여러마리가 있을 수 있다.
-# 상어가 모든 물고기 복제, -> 마지막에 복제되어 칸에 나타남
-# 모든 물고기가 한칸 이동, 상어나 물고기냄새가 있는 칸 제외, 못가면 45도 반시계방향 회전, 없으면 이동안함
-# 상어가 3칸 이동, 상하좌우로 이동 가능, 물고기 있는 칸으로 가면, 물고기는 죽음 -> 죽으면서 냄새 남긴다. -> 가장 많은 물고기가 죽은 칸으로 이동
-# 냄새가 사라짐 (2번 전에 났던 냄새들 삭제)
-# 복제완료, 위치와 방향을 그대로 가져옴
-
-# 물고기의 이동 방향
-fdx = [0, -1, -1, -1, 0, 1, 1, 1] # 감소할수록 반시계
-fdy = [-1, -1, 0, 1, 1, 1, 0, -1]
-
-# 상어의 이동 방향
-sdx = [-1, 0, 1, 0]
-sdy = [0, -1, 0, 1]
-
-m, n = map(int, input().split()) # 물고기의 수 M, 상어가 마법을 연습한 횟수 N
-graph = [[0] * 4 for _ in range(4)] # 4 * 4 그래프에서 움직임
-for _ in range(m): # M개의 물고기 정보
-    x, y, d = map(int, input().split()) # 물고기의 위치 (x, y) 방향 d
+graph = [[[] for _ in range(4)] for _ in range(4)]
+smell = [[0] * 4 for _ in range(4)]
+m, s = map(int, input().split()) # 물고기 수, 상어가 마법을 연습한 횟수
+for _ in range(m):
+    x, y, d = map(int, input().split())
     if graph[x - 1][y - 1]:
         graph[x - 1][y - 1].append(d - 1)
     else:
         graph[x - 1][y - 1] = [d - 1]
+
 sx, sy = map(int, input().split()) # 상어의 위치
 sx, sy = sx - 1, sy - 1
-smell = [[0] * 4 for _ in range(4)]
 
-def fish_move():
+dx = [0, -1, -1, -1, 0, 1, 1, 1]
+dy = [-1, -1, 0, 1, 1, 1, 0, -1]
+
+sdx = [-1, 0, 1, 0]
+sdy = [0, -1, 0, 1]
+
+def fish_move(): # 물고기 이동 시작
     global graph
-    new_graph = [[0] * 4 for _ in range(4)]
+    temp_graph = [[[] for _ in range(4)] for _ in range(4)]
     for x in range(4):
         for y in range(4):
-            if graph[x][y]: # 0이 아닌 물고기 배열이 존재한다면
-                for d in graph[x][y]:
-                    for i in range(8):
-                        nx = x + fdx[(d - i) % 8]
-                        ny = y + fdy[(d - i) % 8]
-                        if 0 <= nx < 4 and 0 <= ny < 4 and smell[nx][ny] == 0 and (nx, ny) != (sx, sy): # 격자를 벗어나지 않고, 냄새가 안나고, 상어의 위치와 다름
-                            if new_graph[nx][ny]:
-                                new_graph[nx][ny].append((d - i) % 8)
+            if graph[x][y]:
+                for now in graph[x][y]:
+                    for d in range(8):
+                        nx = x + dx[now]
+                        ny = y + dy[now]
+                        if 0 <= nx < 4 and 0 <= ny < 4 and smell[nx][ny] == 0 and (nx, ny) != (sx, sy):
+                            if temp_graph[nx][ny]:
+                                temp_graph[nx][ny].append(now)
                             else:
-                                new_graph[nx][ny] = [(d - i) % 8]
+                                temp_graph[nx][ny] = [now]
                             break
-                    else: # 움직일 곳이 없으면
-                        if new_graph[x][y]:
-                            new_graph[x][y].append(d)
                         else:
-                            new_graph[x][y] = [d]
-    graph = new_graph
+                            now = (now - 1) % 8
+                    else:
+                        if temp_graph[x][y]:
+                            temp_graph[x][y].append(now)
+                        else:
+                            temp_graph[x][y] = [now]
+    graph = temp_graph
 
-candi = []
 temp = []
-def make_candi(depth): # 상어가 갈 수 있는 모든 경우의 수를 미리 구해놓는다. 최대 64개라서 오래걸리지 않음
+candi = []
+def make_candi(depth):
     if depth == 3:
         candi.append(list(temp))
         return
+
     for i in range(4):
         temp.append(i)
         make_candi(depth + 1)
@@ -63,63 +57,55 @@ make_candi(0)
 
 def shark_move():
     global sx, sy
-    shark_candi = [] # 상어가 갈 수 있는 곳
-    for idx in range(len(candi)):
-        cnt = 0 # 상어가 먹은 물고기의 수
+    move = [-1, -1] # 제외한 물고기의 수, 사전 순서
+    for idx in range(64):
+        count = 0 # 제외되는 물고기의 수
+        now = candi[idx]
         arr = []
         x, y = sx, sy
-        visit = [[False] * 4 for _ in range(4)]
-        for d in range(3):
-            sd = candi[idx][d]
-            nx = x + sdx[sd]
-            ny = y + sdy[sd]
-            arr.append(sd)
-            if 0 <= nx < 4 and 0 <= ny < 4:
-                if graph[nx][ny]:
-                    if not visit[nx][ny]:
-                        cnt += len(graph[nx][ny])
-                        visit[nx][ny] = True
-                    else:
-                        pass
-                x, y = nx, ny
+        for d in now:
+            x = x + sdx[d]
+            y = y + sdy[d]
+
+            if 0 <= x < 4 and 0 <= y < 4:
+                if graph[x][y] and (x, y) not in arr:
+                    count += len(graph[x][y])
+                    arr.append((x, y))
             else:
                 break
         else:
-            arr.append(cnt)
-            shark_candi.append(arr)
-    shark_candi.sort(key = lambda x : (-x[3], x[0], x[1], x[2])) # 가장 많이 먹은 순, 사전 순서대로 정렬을 해준다.
+            if move[0] < count:
+                move = [count, idx]
 
-    temp = shark_candi[0] # 정렬을 했을때 가장 첫번째값으로 움직이기 시작함
-    for i in range(3):
-        d = temp[i]
+    now = move[1]
+    for d in candi[now]:
         sx = sx + sdx[d]
         sy = sy + sdy[d]
-
         if graph[sx][sy]:
-            smell[sx][sy] = 3 # 잡아먹은 곳 냄새 3로
-            graph[sx][sy] = 0 # 물고기 전부 제외
+            graph[sx][sy] = []
+            smell[sx][sy] = 3
 
 def smell_spread():
     for x in range(4):
         for y in range(4):
-            if smell[x][y]:
+            if smell[x][y] > 0:
                 smell[x][y] -= 1
 
-def add_copyfish():
+def add_fish():
     for x in range(4):
         for y in range(4):
-            if temp_graph[x][y]:
+            if copy_graph[x][y]:
                 if graph[x][y]:
-                    graph[x][y].extend(temp_graph[x][y])
+                    graph[x][y].extend(copy_graph[x][y])
                 else:
-                    graph[x][y] = temp_graph[x][y]
+                    graph[x][y] = copy_graph[x][y]
 
-for _ in range(n):
-    temp_graph = [i[:] for i in graph] # 복제 마법 시전
-    fish_move() # 물고기 한칸 이동
-    shark_move() # 상어 이동
-    smell_spread() # 냄새 1빠짐
-    add_copyfish()
+for _ in range(s):
+    copy_graph = [i[:] for i in graph]
+    fish_move()
+    shark_move()
+    smell_spread()
+    add_fish()
 
 result = 0
 for x in range(4):
