382f9a48e791f0fb7c7bba23ea8b0f335d204f57
anjaeha <vix849@gmail.com>
2022-04-27 20:34:56 +0900

    2022.04.27

--- SWEA/핀볼게임.py
+++ SWEA/핀볼게임.py
@@ -1,90 +1,90 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def move(sx, sy, d):
-    x, y = sx, sy
-    count = 0
-
-    while 1:
-        nx = x + dx[d]
-        ny = y + dy[d]
-
-        if nx < 0 or ny < 0 or nx >= n or ny >= n:
-            return (count * 2 + 1)
-
-        if graph[nx][ny] == -1 or (nx == sx and ny == sy):
-            return count
-
-        if graph[nx][ny] == 0:
-            x, y = nx, ny
-            continue
-        elif graph[nx][ny] == 1:
-            count += 1
-            if d == 1:
-                d = 3
-            elif d == 2:
-                d = 0
-            else:
-                d ^= 1
-        elif graph[nx][ny] == 2:
-            count += 1
-            if d == 0:
-                d = 3
-            elif d == 2:
-                d = 1
-            else:
-                d ^= 1
-        elif graph[nx][ny] == 3:
-            count += 1
-            if d == 0:
-                d = 2
-            elif d == 3:
-                d = 1
-            else:
-                d ^= 1
-        elif graph[nx][ny] == 4:
-            count += 1
-            if d == 1:
-                d = 2
-            elif d == 3:
-                d = 0
-            else:
-                d ^= 1
-        elif graph[nx][ny] == 5:
-            count += 1
-            d ^= 1
-        elif 6 <= graph[nx][ny] <= 10:
-            for rc in holl[graph[nx][ny]]:
-                if nx == rc[0] and ny == rc[1]:
-                    continue
-                nx = rc[0]
-                ny = rc[1]
-                break
-
-        x, y = nx, ny
+from collections import deque
 
 T = int(input())
 
-for tc in range(1, T + 1):
+dx = [-1, 1, 0, 0] # 상 하 좌 우
+dy = [0, 0, -1, 1]
+
+def move(sx, sy, d):
+    q = deque()
+    q.append((sx, sy, d))
+    cnt = 0
+
+    while q:
+        x, y, d = q.popleft()
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[nx][ny] == -1 or (nx, ny) == (sx, sy):
+                return cnt
+
+            if graph[nx][ny] == 0:
+                q.append((nx, ny, d))
+            elif graph[nx][ny] == 1:
+                if d == 0 or d == 3:
+                    return cnt * 2 + 1
+                elif d == 1:
+                    q.append((nx, ny, 3))
+                elif d == 2:
+                    q.append((nx, ny, 0))
+                cnt += 1
+
+            elif graph[nx][ny] == 2:
+                if d == 1 or d == 3:
+                    return cnt * 2 + 1
+                elif d == 0:
+                    q.append((nx, ny, 3))
+                elif d == 2:
+                    q.append((nx, ny, 1))
+                cnt += 1
+
+            elif graph[nx][ny] == 3:
+                if d == 1 or d == 2:
+                    return cnt * 2 + 1
+                elif d == 0:
+                    q.append((nx, ny, 2))
+                elif d == 3:
+                    q.append((nx, ny, 1))
+                cnt += 1
+
+            elif graph[nx][ny] == 4:
+                if d == 0 or d == 2:
+                    return cnt * 2 + 1
+                elif d == 1:
+                    q.append((nx, ny, 2))
+                elif d == 3:
+                    q.append((nx, ny, 0))
+                cnt += 1
+
+            elif graph[nx][ny] == 5:
+                return cnt * 2 + 1
+
+            else:
+                for rr, cc in holl[graph[nx][ny]]:
+                    if (nx, ny) != (rr, cc):
+                        q.append((rr, cc, d))
+        else:
+            return cnt * 2 + 1
+
+for tc in range(T):
     n = int(input())
     graph = [list(map(int, input().split())) for _ in range(n)]
+
     holl = {}
-    zeros = []
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == 0:
-                zeros.append((i, j))
-            elif 6 <= graph[i][j] <= 10:
-                if holl.get(graph[i][j]) is None:
-                    holl[graph[i][j]] = [(i, j)]
+    for x in range(n):
+        for y in range(n):
+            if 6 <= graph[x][y] <= 10:
+                if graph[x][y] in holl:
+                    holl[graph[x][y]].append((x, y))
                 else:
-                    holl[graph[i][j]].append((i, j))
+                    holl[graph[x][y]] = [(x, y)]
 
-    result = 0
-    for x, y in zeros:
-        for d in range(4):
-            temp = move(x, y, d)
-            result = max(result, temp)
+    result = -1
+    for r in range(n):
+        for c in range(n):
+            if graph[r][c] == 0:
+                for d in range(4):
+                    result = max(result, move(r, c, d))
 
-
-    print("#%d %d" %(tc, result))
+    print("#%d %d" %(tc + 1, result))
--- 백준_문제풀이/17142.py
+++ 백준_문제풀이/17142.py
@@ -1,71 +1,65 @@
 from collections import deque
-
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
+# 0 - 빈칸, 1 - 벽, 2 - 바이러스
+
+virus_pos = []
+for x in range(n):
+    for y in range(n):
+        if graph[x][y] == 2:
+            virus_pos.append((x, y))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-vir_pos = []
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 2:
-            vir_pos.append((i, j))
-
 candi = []
 temp = []
-visit = [False] * len(vir_pos)
+visit = [False] * len(virus_pos)
 def make_candi(depth):
     if depth == m:
         candi.append(list(temp))
         return
 
-    for i in range(len(vir_pos)):
+    for i in range(len(virus_pos)):
         if visit[i]:
             continue
+
         visit[i] = True
-        temp.append(vir_pos[i])
+        temp.append(virus_pos[i])
         make_candi(depth + 1)
         temp.pop()
-        for j in range(i + 1, len(vir_pos)):
+        for j in range(i + 1, len(virus_pos)):
             visit[j] = False
+
 make_candi(0)
-
-def check():
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == 0 and count[i][j] == -1:
-                return False
-    return True
-
-result = int(1e9)
+copy_graph = [i[:] for i in graph]
+result = 987654321
 for idx in range(len(candi)):
+    cost = [[-1] * n for _ in range(n)]
     now = candi[idx]
-    q = deque()
-    visit = [[False] * n for _ in range(n)]
-    count = [[-1] * n for _ in range(n)]
-    for x, y in now:
-        count[x][y] = 0
-        q.append((x, y))
-        visit[x][y] = True
+    q = deque(now)
+    for r, c in now:
+        cost[r][c] = 0
     while q:
         x, y = q.popleft()
-
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
-            if 0 <= nx < n and 0 <= ny < n:
-                if not visit[nx][ny] and graph[nx][ny] != 1:
-                    count[nx][ny] = count[x][y] + 1
-                    visit[nx][ny] = True
-                    q.append((nx, ny))
-    if not check():
-        continue
-    MAX = 0
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == 0:
-                MAX = max(MAX, count[i][j])
-    result = min(result, MAX)
 
-print(result if result != int(1e9) else -1)
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] != 1 and cost[nx][ny] == -1:
+                    cost[nx][ny] = cost[x][y] + 1
+                    q.append((nx, ny))
+
+    time = 0
+    flag = False
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] == 0:
+                if cost[x][y] == -1:
+                    flag = True
+                time = max(time, cost[x][y])
+    if not flag:
+        result = min(result, time)
+
+print(result if result != 987654321 else -1)
